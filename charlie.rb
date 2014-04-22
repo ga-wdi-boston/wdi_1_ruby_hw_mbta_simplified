2c3c4c6fc32f36552bd0ecf560f3f4d515907570
Christine Lafontant <cklafontant@gmail.com>
2014-04-22 19:02:06 -0400

    Set-up Task for Subway System
      * Created red line, green line and orange line arrays
      * Created a T line hash
      * Created code for program to determin distance between two stops on the same line

---
+++ charlie.rb
@@ -0,0 +1,96 @@
+#Given this subway system, write a Ruby program that prompts
+#the user for an "origin" stop and a "destination" stop,
+#and tells them the number of stops they would have to make
+#on that journey. For instance, traveling from Central to Copley would
+#be 5 stops.
+
+#(2) Program has the subway system defined with appropriate data structures
+#(1) Program can find the distance between two stops on the same line
+#(2) Program can find the distance between two stops on different lines
+#(1) Program gives the correct distance in edge cases (e.g. route starts and
+  # ends at the same stop, route starts or ends at Park Street)
+#(1) Stops and lines can be added, removed, or rearranged in your code without
+# making any changes to the route-finding logic (within reason – every line
+  # must intersect at a single common stop)
+#(1) Repository has several logical commits with descriptive messages
+#(2) Code uses correct indentation/style and descriptive variable names
+
+
+#Bonus:
+#(1) Add methods to your madness!
+# We haven't fully covered defining your own methods yet,
+# and technically none are required for this assignment.
+# However, you might have noticed some places in your code that
+# are doing the same thing multiple times, but with slightly different data.
+# Introduce a method or two to resolve this duplication.
+
+
+#Define Subway System Array
+# red_line = {
+#   red: "Alewife",
+#   red: "Davis",
+#   red: "Porter",
+#   red: "Harvard",
+#   red: "Central",
+#   red: "Kendal/MIT",
+#   red: "Park Street",
+#   red: "South Station"
+# }
+
+# green_line =  {
+#   green: "Haymarket",
+#   green: "Government Center",
+#   green: "Park Street",
+#   green: "Boylston",
+#   green: "Arlington",
+#   green: "Copley"
+# }
+
+# orange_line = {
+#   orange: "North Station"
+#   orange: "Haymarket"
+#   orange: "Park Street"
+#   orange: "State Street"
+#   orange: "Downtown Crossing"
+#   orange: "Chinatown"
+#   orange: "Tufts Medical Center"
+# }
+
+#Define subway system
+red_line = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendal/MIT", "Park Street", "South Station"]
+
+green_line = ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"]
+
+orange_line = ["North Station", "Haymarket", "Park Street","State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+
+t_lines = {
+      color: "Red",
+      color: "Green",
+      color: "Orange"
+    }
+
+# Asks for station origin
+print "Please enter the name of your station of origin:"
+origin = gets.chomp
+
+print "Please enter the color (Red, Green or Orange) of the train line #{origin} is on:"
+train_line = gets.chomp
+
+
+#Asks for destination
+print "Please enter the name of your destination:"
+destination = gets.chomp
+
+print "Please enter the color (Red, Green or Orange) of the train line #{origin} is on:"
+train_line2 = gets.chomp
+
+# Calculate difference between destinations on same line
+if train_line == "Red"
+    train_line = red_line
+elsif train_line == "Green"
+    train_line = green_line
+else
+    train_line = orange_line
+end
+
+print "Your journey is " + ( red_line.index(destination) - red_line.index(origin) ).to_s  + " stops long."
