faad1401c212dd7e876e4e4e0a44bc71e6f2b1a5
Christine Lafontant <cklafontant@gmail.com>
2014-04-23 00:39:27 -0400

    Cleaned up some syntax errors and added abosolute value method

--- charlie.rb
+++ charlie.rb
@@ -1,67 +1,9 @@
-#Given this subway system, write a Ruby program that prompts
-#the user for an "origin" stop and a "destination" stop,
-#and tells them the number of stops they would have to make
-#on that journey. For instance, traveling from Central to Copley would
-#be 5 stops.
-
-#(2) Program has the subway system defined with appropriate data structures
-#(1) Program can find the distance between two stops on the same line
-#(2) Program can find the distance between two stops on different lines
-#(1) Program gives the correct distance in edge cases (e.g. route starts and
-  # ends at the same stop, route starts or ends at Park Street)
-#(1) Stops and lines can be added, removed, or rearranged in your code without
-# making any changes to the route-finding logic (within reason – every line
-  # must intersect at a single common stop)
-#(1) Repository has several logical commits with descriptive messages
-#(2) Code uses correct indentation/style and descriptive variable names
-
-
-#Bonus:
-#(1) Add methods to your madness!
-# We haven't fully covered defining your own methods yet,
-# and technically none are required for this assignment.
-# However, you might have noticed some places in your code that
-# are doing the same thing multiple times, but with slightly different data.
-# Introduce a method or two to resolve this duplication.
-
-
-#Define Subway System Array
-# red_line = {
-#   red: "Alewife",
-#   red: "Davis",
-#   red: "Porter",
-#   red: "Harvard",
-#   red: "Central",
-#   red: "Kendal/MIT",
-#   red: "Park Street",
-#   red: "South Station"
-# }
-
-# green_line =  {
-#   green: "Haymarket",
-#   green: "Government Center",
-#   green: "Park Street",
-#   green: "Boylston",
-#   green: "Arlington",
-#   green: "Copley"
-# }
-
-# orange_line = {
-#   orange: "North Station"
-#   orange: "Haymarket"
-#   orange: "Park Street"
-#   orange: "State Street"
-#   orange: "Downtown Crossing"
-#   orange: "Chinatown"
-#   orange: "Tufts Medical Center"
-# }
-
 #Define subway system
 red_line = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendal/MIT", "Park Street", "South Station"]
 
-green_line = ["Blank1", "Blank2", "Blank3", "Blank4", "Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"]
+green_line = ["Green1", "Green2", "Green3", "Green4", "Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"]
 
-orange_line = ["Blank1", "Blank2", "Blank3", "Blank4", "North Station", "Haymarket", "Park Street","State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+orange_line = ["Orange1", "Orange2", "Orange3", "Orange4", "North Station", "Haymarket", "Park Street","State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
 
 t_lines = {
       color: "Red",
@@ -85,23 +27,24 @@ print "Please enter the color (Red, Green or Orange) of the train line #{destina
 destination_train_line = gets.chomp
 
 # Assigned the given train line to the appropriate array
-if origin_train_line == "Red"
-    origin_train_line = red_line
-elsif origin_train_line == "Green"
+if origin_train_line == "Green"
     origin_train_line = green_line
-else
+elsif origin_train_line == "Orange"
     origin_train_line = orange_line
-end
-
-if destination_train_line == "Red"
-    destination_train_line = red_line
-elsif destination_train_line == "Green"
-    destination_train_line = green_line
 else
-    destination_train_line = orange_line
+    origin_train_line = red_line
 end
 
-print "Your journey is #{(destination_train_line.index(destination_station) - origin_train_line.index(origin_station)).abs + 1} stops long."
+if destination_train_line == "Green"
+    destination_train_line = green_line
+elsif destination_train_line == "Orange"
+    destination_train_line = orange_line
+else
+    destination_train_line = red_line
+end
+
+print "Your journey is #{(destination_train_line.index(destination_station).to_i - origin_train_line.index(origin_station).to_i).abs} stops long."
+
 
 
 
