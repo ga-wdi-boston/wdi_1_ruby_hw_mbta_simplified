360aafd6df88bcb98be2875d76f7847dcf1e0b84
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-22 20:07:24 -0400

    Create arrays for each subway line

---
+++ hw_mbta.rb
@@ -0,0 +1,8 @@
+green = ['Copley', 'Arlington', 'Boylston', 'Park Street', 'Government Center', 'Haymarket']
+
+red = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall', 'Park Street', 'South Station']
+
+orange = ['Tufts', 'Chinatown', 'Downtown Crossing', 'State Street', 'Park Street', 'Haymarket', 'North Station']
+
+
+puts green.index('Copley') - green.index('Park Street')
