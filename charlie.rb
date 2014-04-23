1aeb66d7f1ea638e2e59a28090f7264b8040814e
Lisa Plesko <plesko.l@gmail.com>
2014-04-22 22:22:37 -0400

    Large update: New hash of MBTA stops
    * Change MBTA data from single array to hash with arrays
    * Calculate distances of stops from Park Street

--- charlie.rb
+++ charlie.rb
@@ -1,97 +1,121 @@
+mbta = {
+        red: ["Alewife",
+           "Davis",
+           "Porter",
+           "Harvard",
+           "Central",
+           "Kendall/MIT",
+           "Park Street",
+           "South Station"],
 
+       green: [
+           "Haymarket",
+           "Government Center",
+           "Park Street",
+           "Boylston",
+           "Arlington",
+           "Copley"],
 
-red_line = [
-           "alewife",
-           "davis",
-           "porter",
-           "harvard",
-           "central",
-           "kendall/mit",
-           "park street",
-           "south station"
-         ]
-
-green_line = [
-           "haymarket",
-           "government center",
-           "park street",
-           "boylston",
-           "arlington",
-           "copley"
-         ]
-
-orange_line = [
-           "north station",
-           "haymarket",
-           "park street",
-           "state street",
-           "downtown crossing",
-           "chinatown",
-           "tufts medical center"
-         ]
-
-
-subway = [red_line, green_line, orange_line]
-
+       orange: [
+           "North Station",
+           "Haymarket",
+           "Park Street",
+           "State Street",
+           "Downtown Crossing",
+           "Chinatown",
+           "Tufts Medical Center"]
+}
 
 
 # User selects origin => need to select line as well!
 puts "Welcome to the MBTA!"
-print "What line are you riding? "
-
-while true
-
-  line = gets.chomp.downcase
-
-  if line.include?("red")
-    line = red_line
-    break
-  elsif line.include?("orange")
-    line = orange_line
-    break
-  elsif line.include?("green")
-    line = green_line
-    break
-  else
-    puts "Are you sure you're in Boston? We don't have that line!"
-    print "Please select the red line, green line or orange line. "
-  end
-
-end
-
-print "Starting Station: "
-origin = gets.chomp.downcase
+print "What is your departing station? "
+origin = gets.chomp
 
 
-=begin
 # Validation to clarify which Haymarket
-if origin == "Haymarket"
-  print "Are you on the green line or orange line? "
-  line = gets.chomp.downcase
+# if origin == "Haymarket"
+#   print "Are you on the green line or orange line? "
+
+# end
+
+# if line.include?("green")
+#   origin = "Haymarket_Green"
+#   green_line[0] = "Haymarket_Green"  # will not work if order change
+# elsif line.include?("orange")
+#   origin = "Haymarket_Orange"
+#   orange_line[1] = "Haymarket_Orange" # will not work if order change
+# end
+
+
+
+if mbta[:red].include?(origin)
+  origin_line = :red
+  origin_num = mbta[:red].index(origin)
+  to_park = mbta[:red].index("Park Street")
+  origin_distance = (to_park - origin_num).abs   # distance current line
 end
 
-if line.include?("green")
-  origin = "Haymarket_Green"
-  green_line[0] = "Haymarket_Green"  # will not work if order change
-elsif line.include?("orange")
-  origin = "Haymarket_Orange"
-  orange_line[1] = "Haymarket_Orange" # will not work if order change
+if mbta[:green].include?(origin)
+  origin_line = :green
+  origin_num = mbta[:green].index(origin)
+  to_park = mbta[:green].index("Park Street")
+  origin_distance = (to_park - origin_num).abs  # distance current line
 end
-=end
+
+if mbta[:orange].include?(origin)
+  origin_line = :orange
+  origin_num = mbta[:orange].index(origin)
+  to_park = mbta[:orange].index("Park Street")
+  origin_distance = (to_park - origin_num).abs   # distance current line
+end
 
 
 # User selects destination
 print "Destination: "
-destination = gets.chomp.downcase
+destination = gets.chomp
 
-# Assign number for locations
-start_num = line.index(origin)
-finish_num = line.index(destination)
+
+if mbta[:red].include?(destination)
+  destination_line = :red
+  destination_num = mbta[:red].index(destination)
+  from_park = mbta[:red].index("Park Street")
+  destination_distance = (from_park - destination_num).abs   # distance current line
+end
+
+if mbta[:green].include?(destination)
+  destination_line = :green
+  destination_num = mbta[:green].index(destination)
+  from_park = mbta[:green].index("Park Street")
+  destination_distance = (from_park - destination_num).abs   # distance current line
+end
+
+if mbta[:orange].include?(destination)
+  destination_line = :orange
+  destination_num = mbta[:orange].index(destination)
+  from_park = mbta[:orange].index("Park Street")
+  destination_distance = (from_park - destination_num).abs   # distance current line
+end
 
 # Subtract location numbers to find stop difference
-stops = (finish_num - start_num).abs
+stops = origin_distance + destination_distance
 
-puts stops
+if stops == 0
+  puts "You have arrived at your destination!"
+else
+  puts "You have #{stops} stops until you arrive at your destination!"
+end
+
+
+
+
+
+
+
+# Assign number for locations
+# destination_num = mbta[destination_line].index(destination)
+
+
 
 # If staying on the same line:
 
