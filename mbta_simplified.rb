cc482086513a6d800c963f9d8c4ac4ce095b0fdc
krk324 <krk32488@gmail.com>
2014-06-10 19:03:52 -0400

    Code can now find out distance between same line.

--- mbta_simplified.rb
+++ mbta_simplified.rb
@@ -2,10 +2,10 @@
 
 #Data structures for mbta
 mbta = {
-  redline: {station: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']},
-  greenline: {station: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington','Copley'] },
-  orangeline: {station: ['North Station','Haymarket','Park Street','State Street','Downtown Crossing','Chinatown','Tufts Medical Center'] },
-  intersection: {station: ['Park Street']}
+  redline: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station'],
+  greenline: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington','Copley'],
+  orangeline: ['North Station','Haymarket','Park Street','State Street','Downtown Crossing','Chinatown','Tufts Medical Center'],
+  intersection: ['Park Street']
 }
 
 puts "Enter your origin line"
@@ -18,6 +18,23 @@ puts "Enter your destination stop"
 destination_stop = 'Porter'
 
 
-origin_index =
+#This method find index of each station
+def station_index(line, stop, mbta)
+  stations = mbta[line]
+  puts stations
+  puts stations.index(stop)
+  return stations.index(stop)
+end
+
+origin_stop_index = station_index(origin_line,origin_stop,mbta)
+
+destination_stop_index = station_index(destination_line,destination_stop,mbta)
+
+distance = origin_stop_index + destination_stop_index
+
+puts "Traveling from #{origin_stop} to #{destination_stop} is #{distance} stop(s)."
+
+
+
 
 
