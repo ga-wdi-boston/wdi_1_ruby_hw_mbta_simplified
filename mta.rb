a5bba318e7058150a99bf64ec994617c133b39a5
Jodi Detch <jodidetch@gmail.com>
2014-06-10 19:00:24 -0400

    Added variables for d & 0 lines, d & o stops & how to calc number of stops on same line

--- mta.rb
+++ mta.rb
@@ -1,7 +1,35 @@
-red = %w(Alewife Davis Porter Harvard Central Kendall Park South)
-green = %w(Copley Arlington Boylston Park Government Haymarket)
-orange = %w(Tufts Chinatown Downtown State Park Haymarket North)
+require 'pry'
 
+red = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall', 'Park', 'South Station']
+green = ['Copley', 'Arlington', 'Boylston', 'Park Street', 'Government Center', 'Haymarket']
+orange = ['Tufts', 'Chinatown', 'Downtown Crossing', 'State Street', 'Park Street', 'Haymarket', 'North Station']
+
+lines = {
+  red: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall', 'Park', 'South Station'],
+  green: ['Copley', 'Arlington', 'Boylston', 'Park Street', 'Government Center', 'Haymarket'],
+  orange: ['Tufts', 'Chinatown', 'Downtown Crossing', 'State Street', 'Park Street', 'Haymarket', 'North Station']
+}
+# print "Enter the origin line: "
+# origin_line = gets.chomp.to_sym
+origin_line = "red".to_sym
+
+# print "Enter the origin stop: "
+# origin_stop = gets.chomp.to_sym
+origin_stop = "Davis"
+
+destination_line = 'red'.to_sym
+destination_stop = "Kendall"
+
+
+# lines[origin_line] returns the array of stops on given line
+# index(origin_stop) searches for the index of the origin_stop provided
+origin_index = lines[origin_line].index(origin_stop)
+destination_index = lines[destination_line].index(destination_stop)
+
+# simple case, both origin and destination stops are on the same line.
+number_of_stops = (origin_index - destination_index).abs
+
+binding.pry
 # origin_line = gets.chomp
 # origin_stop = gets.chomp
 
