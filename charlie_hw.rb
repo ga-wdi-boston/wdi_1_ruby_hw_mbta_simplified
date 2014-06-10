cd29f32ef308dc19ba7d16dba6340b659dcba24f
testkitschen <katryn.mcintosh@gmail.com>
2014-06-10 18:44:06 -0400

    Convert to hashes
    Convert arrays that represent each of the lines to hashes so that the keys are the colors of the lines. add method to line responses to downcase and convert to symbol.

--- charlie_hw.rb
+++ charlie_hw.rb
@@ -1,17 +1,17 @@
 subway = [
-  ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "Boylston"],
-  ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
-  ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+  {red: ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "Boylston"]},
+  {green: ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"]},
+  {orange: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]}
 ]
 
 puts "What stop are you starting at?"
 stop_origin = gets.chomp
 
 puts "And which line is #{stop_origin} on?"
-line_origin = gets.chomp
+line_origin = gets.chomp.downcase.to_sym
 
 puts "Which stop do you need to go?"
 stop_final = gets.chomp
 
 puts "And which line is #{stop_final} on?"
-line_final = gets.chomp
+line_final = gets.chomp.downcase.to_sym
