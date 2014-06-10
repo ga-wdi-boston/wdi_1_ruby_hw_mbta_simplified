541956d4362f199a8567bbed77fc90da3526cd51
dmcgann1 <dmcgann1@gmail.com>
2014-06-10 18:07:21 -0400

    Hard coded method to calculate number of stops if on same line

--- subway.rb
+++ subway.rb
@@ -1,7 +1,11 @@
 # Initial data structure for subway system
 
 subway = {
-  green_line: ["Haymarket", 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley'],
-  red_line: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
+  green_line: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley'],
+  red_line: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station'],
   orange_line: ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
 }
+
+# Calculation for distance between stops
+
+puts distance = (subway[:green_line].index("Park Street") - subway[:green_line].index("Haymarket")).abs
