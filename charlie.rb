09a1a6d9d5108c018149bebf26e434341c5dd0d0
Emma Gardner <emmagardner@outlook.com>
2014-06-10 18:12:46 -0400

    create data structure for subway system
    * Create a hash to represent the entire subway system
    * Create hash entries using the line (red, green, orange) names as symbols/keys and the values are arrays with the associated stops listed in order.

---
+++ charlie.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+# Program has the subway system defined with appropriate data structures
+mbta = {
+  red: ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
+  green: ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
+  orange: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+}
+
+binding.pry
+
+# Program can find the distance between two stops on the same line
+
+# Program can find the distance between two stops on different lines
+
+# Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
+
+# Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason – every line must intersect at a single common stop)
+
+# Repository has several logical commits with descriptive messages
+
+# Code uses correct indentation/style and descriptive variable names
