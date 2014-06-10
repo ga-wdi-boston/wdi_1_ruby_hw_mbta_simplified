cdfa0517886f97b93ecb6c318af9f62572a00753
Jake Austin <jakeaust07@gmail.com>
2014-06-10 18:09:21 -0400

    Hard-coded case for traveling along one line

--- metro.rb
+++ metro.rb
@@ -1,4 +1,21 @@
-red = %w(alewife davis porter harvard central kendall park south)
-green = %w(haymarket government park boylston arlington copley)
-orange = %w(north haymarket park state downtown chinatown tufts)
+metro = {
+  red:  %w(alewife davis porter harvard central kendall park south),
+  green: %w(haymarket government park boylston arlington copley),
+  orange: %w(north haymarket park state downtown chinatown tufts)
+}
 
+start_line = :green
+end_line = :green
+start_station = "haymarket"
+end_station = "copley"
+
+distance = -1
+
+# If start-line and end-line are the same
+if start_line == end_line
+  distance = (metro[start_line].index(start_station) - metro[start_line].index(end_station)).abs
+else
+  # handle crossing to a different start_line
+end
+
+puts distance
