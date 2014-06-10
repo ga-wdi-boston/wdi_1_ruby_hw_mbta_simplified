3f56d35266d9c5bd320254793aa08b928dfd123b
krk324 <krk32488@gmail.com>
2014-06-10 18:21:24 -0400

    Add data structure

--- mbta_simplified.rb
+++ mbta_simplified.rb
@@ -1,2 +1,9 @@
 # This program will ask users for users origin and destination stop and inform them the distance between the stops.
 
+#Data structures for mbta
+mbta = {
+  redline: {station: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']},
+  greenline: {station: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington','Copley'] },
+  orangeline: {station: ['North Station','Haymarket','Park Street','State Street','Downtown Crossing','Chinatown','Tufts Medical Center'] },
+  intersection: {station: ['Park Street']}
+}
