fe89efe0e68e24cfa77a6059e8f6dc82d2e3444c
dmcgann1 <dmcgann1@gmail.com>
2014-06-10 17:58:28 -0400

    Data structure for subway system

---
+++ subway.rb
@@ -0,0 +1,7 @@
+# Initial data structure for subway system
+
+subway = {
+  green_line: ["Haymarket", 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley'],
+  red_line: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
+  orange_line: ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
+}
