05945bbe56e3e5bd5e3cd9ffc037a5a934175273
brimatteng <btenggren20@gmail.com>
2014-06-10 19:31:05 -0400

    Fixes distance formulas and .capitalize problems. Fully functional.

--- hw2.rb
+++ hw2.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #create each individual line
 red_line = ['Alewife', 'Davis', 'Porter','Harvard','Central', 'Kendall/MIT', 'Park Street','South Station' ]
 orange_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
@@ -40,29 +41,30 @@ end
 
 #gets final stop
 puts "What is your final stop? (Use capital letters): "
-d_stop = gets.chomp.capitalize
+d_stop = gets.chomp
 #checks to see if it's a valid stop
 while !mbta_lines[d_line].index(d_stop)
   puts "Sorry not a valid stop on this line. Try again: "
-  d_stop = gets.chomp.capitalize
+  d_stop = gets.chomp
+  binding.pry
 end
 
 puts "Your starting line is #{o_line} and your starting stop is #{o_stop}"
 puts "Your destination line is #{d_line} and your finishing stop is #{d_stop}"
 
-
-=begin
+#find distances -----------------------------------
 #find distance between two points on the same line
 if o_line == d_line
-  same_line_dist = (o_line.index(o_stop) - o_line.index(d_stop)).abs
-  #puts same_line_dist
+  same_line_dist = (mbta_lines[o_line].index(o_stop) - mbta_lines[o_line].index(d_stop)).abs
+  puts "The total number of stops will be: #{same_line_dist}"
 end
+
 
 #find distance between two points on different lines
 if o_line != d_line
-  same_line_dist = (o_line.index(o_stop) - o_line.index(meet_point)).abs
-  other_line_dist = (d_line.index(meet_point) - d_line.index(d_stop)).abs
+  same_line_dist = (mbta_lines[o_line].index(o_stop) - mbta_lines[o_line].index(meet_point)).abs
+  other_line_dist = (mbta_lines[d_line].index(meet_point) - mbta_lines[d_line].index(d_stop)).abs
   total_dist = same_line_dist + other_line_dist
-  #puts total_dist
+  puts "The total number of stops will be: #{total_dist}"
 end
-=end
+
