41d211f357fff8897593af3c31b26c2015731cad
Jake Austin <jakeaust07@gmail.com>
2014-06-10 18:17:06 -0400

    abstract intersection, add to metro structure

--- metro.rb
+++ metro.rb
@@ -1,13 +1,14 @@
 metro = {
   red:  %w(alewife davis porter harvard central kendall park south),
   green: %w(haymarket government park boylston arlington copley),
-  orange: %w(north haymarket park state downtown chinatown tufts)
+  orange: %w(north haymarket park state downtown chinatown tufts),
+  intersection: "park"
 }
 
 start_line = :orange
-end_line = :green
-start_station = "haymarket"
-end_station = "copley"
+end_line = :red
+start_station = "tufts"
+end_station = "alewife"
 
 distance = -1
 
@@ -16,8 +17,8 @@ if start_line == end_line
   distance = (metro[start_line].index(start_station) - metro[start_line].index(end_station)).abs
 else
   # handle crossing to a different start_line
-  distance_to_park = (metro[start_line].index(start_station) - metro[start_line].index('park')).abs
-  distance_from_park = (metro[end_line].index(end_station) - metro[end_line].index('park')).abs
+  distance_to_park = (metro[start_line].index(start_station) - metro[start_line].index(metro[:intersection])).abs
+  distance_from_park = (metro[end_line].index(end_station) - metro[end_line].index(metro[:intersection])).abs
   distance = distance_to_park + distance_from_park
 end
 
