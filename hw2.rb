0abf006aa24c155047e329262f117a941ece6e80
brimatteng <btenggren20@gmail.com>
2014-06-10 18:38:14 -0400

    Finds distance to other train lines.

--- hw2.rb
+++ hw2.rb
@@ -5,15 +5,24 @@ green_line = ['Haymarket','Government Center', 'Park Street', 'Boylston', 'Arlin
 
 # create mbta system
 mbta_lines = [red_line, orange_line, green_line]
+meet_point = 'Park Street'
 
 #user input variables
 o_line = red_line
 o_stop = 'Alewife'
-d_line = red_line
-d_stop = 'Park Street'
+d_line = orange_line
+d_stop = 'Chinatown'
 
 #find distance between two points on the same line
 if o_line == d_line
   same_line_dist = (o_line.index(o_stop) - o_line.index(d_stop)).abs
-  puts same_line_dist
+  #puts same_line_dist
+end
+
+#find distance between two points on different lines
+if o_line != d_line
+  same_line_dist = (o_line.index(o_stop) - o_line.index(meet_point)).abs
+  other_line_dist = (d_line.index(meet_point) - d_line.index(d_stop)).abs
+  total_dist = same_line_dist + other_line_dist
+  puts total_dist
 end
