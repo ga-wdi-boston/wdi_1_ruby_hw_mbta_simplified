610a4f322f5c78a5a95d8804dd3b51d448fca6bf
Devin Dreszer <devindreszer@gmail.com>
2014-06-10 18:02:44 -0400

    Find distance between stops on same line

--- subway.rb
+++ subway.rb
@@ -5,6 +5,13 @@ subway = {
   orange_line: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
 }
 
+# initialize variables for testing
+origin_stop = "Copley"
+origin_line = :green_line
+destination_stop = "Government Center"
+destination_line = :green_line
 
+# num stops on same line
+num_stops = (subway[origin_line].index(origin_stop) - subway[destination_line].index(destination_stop)).abs
 
-# subway[:green_line].index("Park Street") - subway[:green_line].index("Haymarket")
+puts num_stops
