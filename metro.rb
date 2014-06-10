2be81da63be9cd7d7d4065a73a78ae9b7e67c6f2
Jake Austin <jakeaust07@gmail.com>
2014-06-10 18:14:23 -0400

    Hard-coded case for traveling across different lines

--- metro.rb
+++ metro.rb
@@ -4,7 +4,7 @@ metro = {
   orange: %w(north haymarket park state downtown chinatown tufts)
 }
 
-start_line = :green
+start_line = :orange
 end_line = :green
 start_station = "haymarket"
 end_station = "copley"
@@ -16,6 +16,9 @@ if start_line == end_line
   distance = (metro[start_line].index(start_station) - metro[start_line].index(end_station)).abs
 else
   # handle crossing to a different start_line
+  distance_to_park = (metro[start_line].index(start_station) - metro[start_line].index('park')).abs
+  distance_from_park = (metro[end_line].index(end_station) - metro[end_line].index('park')).abs
+  distance = distance_to_park + distance_from_park
 end
 
 puts distance
