d459fca53901e61ebc4978ab2289f6ab64317b49
Jake Austin <jakeaust07@gmail.com>
2014-06-10 17:59:07 -0400

    declare lines as array of stations

---
+++ metro.rb
@@ -0,0 +1,4 @@
+red = %w(alewife davis porter harvard central kendall park south)
+green = %w(haymarket government park boylston arlington copley)
+orange = %w(north haymarket park state downtown chinatown tufts)
+
