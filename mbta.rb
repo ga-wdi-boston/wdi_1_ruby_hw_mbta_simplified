4b17b59f415a8de3b6c3ddb885c89733d4fbe492
csb324 <csb324@gmail.com>
2014-06-10 18:03:34 -0400

    Erase previous stuff. Set up charlie map.

--- mbta.rb
+++ mbta.rb
@@ -1,25 +1,9 @@
-reds = %w(Alewife Davis Porter Harvard Central Kendall/MIT ParkStreet SouthStation)
-greens = %w(Haymarket GovernmentCenter ParkStreet Boylston Arlington Copley)
-oranges = %w(NorthStation Haymarket ParkStreet StateSt Downtown Chinatown TuftsMedical)
+require 'pry'
 
+charlie_map = {
+  :redline => %w(Alewife Davis Porter Harvard Central Kendall/MIT ParkStreet SouthStation),
+  :greenline => %w(Haymarket GovernmentCenter ParkStreet Boylston Arlington Copley),
+  :orangeline => %w(NorthStation Haymarket ParkStreet StateSt Downtown Chinatown TuftsMedicalCenter)
+}
 
-puts "ORIGIN INFORMATION"
-
-origin_line = nil
-
-while origin_line == nil do
-  puts "Line:"
-  input = gets.chomp.downcase
-  if input == "red"
-    origin_line = reds
-  elsif input == "green"
-    origin_line = greens
-  elsif origin_line == "orange"
-    origin_line = oranges
-  else
-    puts "Enter red, green, or orange line"
-  end
-end
-
-origin_station = nil
-
+binding.pry
