fce8dabb6721712ac6791e3b43e2fe97c434342d
krk324 <krk32488@gmail.com>
2014-06-10 18:34:09 -0400

    Add testcase

--- mbta_simplified.rb
+++ mbta_simplified.rb
@@ -7,3 +7,13 @@ mbta = {
   orangeline: {station: ['North Station','Haymarket','Park Street','State Street','Downtown Crossing','Chinatown','Tufts Medical Center'] },
   intersection: {station: ['Park Street']}
 }
+
+puts "Enter your origin line"
+origin_line = 'redline'.to_sym
+puts "Enter your origin stop"
+origin_line = 'Alewife'
+puts "Enter your destination line"
+origin_line = 'redline'.to_sym
+puts "Enter your destination stop"
+origin_line = 'Porter'
+
