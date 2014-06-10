e8b826efe105f1e2472bb24e7308e791a803ce3c
Devin Dreszer <devindreszer@gmail.com>
2014-06-10 18:16:20 -0400

    Finds distance bewteen stops on different lines

--- subway.rb
+++ subway.rb
@@ -6,12 +6,26 @@ subway = {
 }
 
 # initialize variables for testing
-origin_stop = "Copley"
-origin_line = :green_line
-destination_stop = "Government Center"
-destination_line = :green_line
+origin_stop = "South Station"
+origin_line = :red_line
+destination_stop = "Chinatown"
+destination_line = :orange_line
+
+# set the intersection stop
+intersection_stop = "Park Street"
+
+#initialize num_stops to zero
+num_stops = 0
+
+# num stops on different lines
+if origin_line != destination_line
+  num_stops += (subway[origin_line].index(origin_stop) - subway[origin_line].index(intersection_stop)).abs
+  num_stops += (subway[destination_line].index(intersection_stop) - subway[destination_line].index(destination_stop)).abs
 
 # num stops on same line
-num_stops = (subway[origin_line].index(origin_stop) - subway[destination_line].index(destination_stop)).abs
+else
+  num_stops += (subway[origin_line].index(origin_stop) - subway[destination_line].index(destination_stop)).abs
+end
+
 
 puts num_stops
