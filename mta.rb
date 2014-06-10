89a4fb4b2eaeb0791b7d44e610ec27c6d012f193
Jodi Detch <jodidetch@gmail.com>
2014-06-10 18:22:24 -0400

    Program has the subway system defined with appropriate data structures

---
+++ mta.rb
@@ -0,0 +1,13 @@
+red = %w(Alewife Davis Porter Harvard Central Kendall Park South)
+green = %w(Copley Arlington Boylston Park Government Haymarket)
+orange = %w(Tufts Chinatown Downtown State Park Haymarket North)
+
+# origin_line = gets.chomp
+# origin_stop = gets.chomp
+
+#puts "Please enter your origin line #{origin_line} and your origin_stop #{origin_stop}:"
+
+# destination_line = gets.chomp
+# destination_stop = gets.chomp
+
+#puts "Please enter your destination line #{destination_line} and your destination_stop #{destination_stop}:"
