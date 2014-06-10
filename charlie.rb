7dcca9c8e11d4c907bec51df3fb8a64083a0c864
Emma Gardner <emmagardner@outlook.com>
2014-06-10 19:06:28 -0400

    use user inputs to calculate stop distance on same line
    * use .index to access the index location of the stop on the particular line
    
    * subtract origin index from destination index and use the absolute value as the number of stops

--- charlie.rb
+++ charlie.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 # Program has the subway system defined with appropriate data structures
 mbta = {
-  red: ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
-  green: ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
-  orange: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
+  red: ["alewife", "davis", "porter", "harvard", "central", "kendall/MIT", "park street", "south station"],
+  green: ["haymarket", "government center", "park street", "boylston", "arlington", "copley"],
+  orange: ["north station", "haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]
 }
 
 binding.pry
@@ -13,7 +13,7 @@ binding.pry
 
 puts "What is your origin MBTA line?"
 
-o_line = gets.chomp.downcase
+o_line = gets.chomp.downcase.to_sym
 
 puts "What is your origin MBTA stop?"
 
@@ -21,24 +21,31 @@ o_stop = gets.chomp.downcase
 
 puts "What is your desitination line?"
 
-d_line = gets.chomp.downcase
+d_line = gets.chomp.downcase.to_sym
 
 puts "What is your destination stop?"
 
 d_stop = gets.chomp.downcase
 
+# Take the user inputs and find/return the distance between the two stops on the same line
+
+# will list the index location of the stop on the particular line
+
+origin_index = mbta[o_line].index(o_stop)
+
+dest_index = mbta[d_line].index(d_stop)
+
+same_line_stops = origin_index - dest_index
+
+#puts "It will take to #{same_line_stops.abs} to get from #{o_stop} to #{d_stop}."
+
+binding.pry
 
 
-
-
-
-
-
-
-
-# Program can find the distance between two stops on the same line
 
 # Program can find the distance between two stops on different lines
+
+
 
 # Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
 
