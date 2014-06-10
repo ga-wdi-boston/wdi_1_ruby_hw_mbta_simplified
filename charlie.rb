8fdb41d73982d5bc5d6ae11abefe36eae22d3d35
sarahchristine913 <s.mele13@gmail.com>
2014-06-10 17:59:07 -0400

    Created file charlie.rb
    * Added initial data structure, arrays for each line
    * Added array of all lines

---
+++ charlie.rb
@@ -0,0 +1,9 @@
+
+
+red_line = %w(Alewife, Davis, Porter, Harvard, Central, Kendall, Park, South Station)
+
+green_line = %W(Haymarket, Government Center, Park, Boylston, Arlington, Copley)
+
+orange_line = %w(North Station, Haymarket, Park, State Street, Downtown, Chinatown, Tufts)
+
+mta_lines = [red_line, green_line, orange_line]
