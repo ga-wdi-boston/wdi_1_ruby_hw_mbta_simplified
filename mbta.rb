dbb278e74d6a5e9e7344eaea612117b47336b6c1
zhbanton <zhbanton@gmail.com>
2014-06-10 18:57:40 -0400

    Create Data Structure and tracking variables

---
+++ mbta.rb
@@ -0,0 +1,13 @@
+mbta =
+{red_line: ["Alewife", "Davis", "Porter", "Harvard", "Central",
+            "Kendall/MIT", "Park Street", "South Station"],
+green_line: ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
+orange_line: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing",
+"Chinatown", "Tufts Medical Center"]}
+
+CROSSROADS = "Park Street" # intersection point of all lines
+
+origin_line = :red_line
+origin_stop = "Central"
+destination_line = :red_line
+destination_stop = "South Station"
