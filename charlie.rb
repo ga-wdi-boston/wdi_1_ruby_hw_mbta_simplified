34eef6a353174dbacd791450d2be3cb2dd99db41
Julie <juliesof@gmail.com>
2014-06-10 20:50:39 -0400

    Changed data structure of subway
    *From array to a hash holding 3 arrays
    *line colors are the keys
    *stops are the values

--- charlie.rb
+++ charlie.rb
@@ -1,4 +1,11 @@
-red = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
-green = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
-orange = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical School']
+mbta = {
+  red: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station'],
+  green: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley'],
+orange: ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical School']
+}
+
+origin_line = 'orange'
+origin_stop = 'Chinatown'
+dest_line = 'orange'
+dest_stop = 'North Station'
 
