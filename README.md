3707ea967bb1d8fe8061deaf47600619d0ca26d1
Julie <juliesof@gmail.com>
2014-06-11 00:21:33 -0400

    Added if/else expressions to show list of stations when lines are switched
    *Can't tell if this is functional because of unexpected end-of-input error.
    *Terminal says it expects as "end" but still errors out when "end" is on this line

--- README.md
+++ README.md
@@ -41,11 +41,11 @@ Refer to the instructions in the [Guess the Number homework](https://github.com/
 
 ## Specifications
 
-1 Program has the subway system defined with appropriate data structures
-2 Program can find the distance between two stops on the same line
+1 check Program has the subway system defined with appropriate data structures
+2 check Program can find the distance between two stops on the same line
 3 Program can find the distance between two stops on different lines
-4 Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
-5 Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason &ndash; every line must intersect at a single common stop)
+4 Program gives the correct distance in edge cases (e.g. CHECK route starts and ends at the same stop, route starts or ends at Park Street)
+5 check Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason &ndash; every line must intersect at a single common stop)
 * Repository has several logical commits with descriptive messages
 * Code uses correct indentation/style and descriptive variable names
 
--- charlie.rb
+++ charlie.rb
@@ -1,5 +1,4 @@
 
-
 mbta = {
   red: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT',
     'Park Street', 'South Station'],
@@ -8,26 +7,71 @@ mbta = {
   orange: ['North Station', 'Haymarket', 'Park Street', 'State Street','Downtown Crossing', 'Chinatown', 'Tufts Medical School']
 }
 
-#lines will be the sym to use as keys in the mbta hash
-origin_line = :orange #user input **.to_sym
-origin_stop = mbta[origin_line].index('Chinatown') #will be var from user
-origin = [origin_line, origin_stop]
-dest_line = :orange #user input
-dest_stop = mbta[dest_line].index('North Station') #user input
-dest = [dest_line, dest_stop]
+puts "On which color line will you beginning your trip today?"
+origin_line = gets.chomp.to_sym
+
+puts "At which station will you be beginning your trip?"
+origin_station = gets.chomp
+origin_stop = mbta[origin_line].index(origin_station)
+
+puts "On which color line will you ending your trip today?"
+dest_line = dest_line.to_sym
+puts "At which station will you be ending your trip?"
+dest_station = gets.chomp
+dest_stop = mbta[dest_line].index(dest_station)
+
+switch_lines_stop = 'Park Street'
 
 station_cntr = origin_stop #better named var for a cntr
-if origin_stop > dest_stop #ex 4..0
-  while station_cntr >= dest_stop #cntr starts at 4
-    puts mbta[origin_line][station_cntr] #var[key][array index]
-    station_cntr -= 1
+
+if origin_stop == dest_stop
+  puts "Your first and last stop is #{mbta[origin_line][station_cntr]}"
+end
+
+if origin_line == dest_line
+  #origin and dest are on same line
+  if origin_stop > dest_stop #ex 4..0
+    while station_cntr > dest_stop #cntr starts at 4
+      puts mbta[origin_line][station_cntr] #var[key][array index]
+      station_cntr -= 1
+    end
+  else origin_stop < dest_stop #ex 0..4
+    while station_cntr < dest_stop #cntr starts at 0 & inc to dest_stop
+      puts mbta[origin_line][station_cntr] #var[key][array index]
+      station_cntr += 1
+    end
   end
-elsif origin_stop < dest_stop #ex 0..4
-  while station_cntr <= dest_stop #cntr starts at 0 & inc to dest_stop
+end
+
+
+if origin_line != dest_line
+  if origin_stop > dest_stop #ex 4..0
+    while station_cntr > dest_stop #cntr starts at 4
+      puts mbta[origin_line][station_cntr]
+      station_cntr -= 1
+    until mbta[origin_line][mbta[origin_line].index(switch_lines_stop -1)]
+
+    puts mbta[dest_line][mbta[origin_line].index(switch_lines_stop)]
+    station_cntr_2 = switch_lines_stop + 1
+
+    while station_cntr_2 > dest_stop
+      puts mbta[origin_line][station_cntr] #var[key][array index]
+      station_cntr -= 1
+    end
+ end
+end
+
+if origin_stop < dest_stop #ex 0..4
+  while station_cntr < dest_stop #cntr starts at 0 & inc to dest_stop
+    puts mbta[origin_line][station_cntr] #var[key][array index]
+    station_cntr += 1
+   until mbta[origin_line][mbta[origin_line].index(switch_lines_stop -1)]
+
+   puts mbta[dest_line][mbta[origin_line].index(switch_lines_stop)]
+   station_cntr_2 = switch_lines_stop + 1
+
+   while station_cntr_2 < dest_stop
     puts mbta[origin_line][station_cntr] #var[key][array index]
     station_cntr += 1
   end
 end
-
-#do starting at origin_stop{ |var| } until dest_stop
-
