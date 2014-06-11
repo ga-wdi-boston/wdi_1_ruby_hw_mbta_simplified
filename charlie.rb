3e54c4c6ccf05c1a5e9e0a978e02a4911781ba1e
Max Kohl <maxkohl810@gmail.com>
2014-06-11 07:38:38 -0400

    find all stops that are on the red line

--- charlie.rb
+++ charlie.rb
@@ -1,47 +1,59 @@
 require 'pry'
 
-# All stops
-# stops = {
-#   alewife: {line: 'red'},
-#   davis: {line: 'red'},
-#   porter: {line: 'red'},
-#   harvard: {line: 'red'},
-#   central: {line: 'red'},
-#   kendall: {line: 'red'},
-#   park_street: {line: 'red'},
-#   south_station: {line: 'red'},
-#   haymarket: [line: 'green'],
-#   government_center: {line: 'green'},
-#   park_street: {line: 'green'},
-#   boylston: {line: 'green'},
-#   arlington: {line: 'green'},
-#   copley: {line: 'green'},
-#   north_station: {line: 'orange'},
-#   haymarket: {line: 'orange'},
-#   park_street: {line: 'orange'},
-#   state_street: {line: 'orange'},
-#   downtown_crossing: {line: 'orange'},
-#   chinatown: {line: 'orange'},
-#   tufts_medical_center: {line: 'orange'}
-# }
+#All stops
+stations = {
+  alewife: {line: 'red'},
+  davis: {line: 'red'},
+  porter: {line: 'red'},
+  harvard: {line: 'red'},
+  central: {line: 'red'},
+  kendall: {line: 'red'},
+  park_street: {line: 'red'},
+  south_station: {line: 'red'},
+  haymarket: [line: 'green'],
+  government_center: {line: 'green'},
+  park_street: {line: 'green'},
+  boylston: {line: 'green'},
+  arlington: {line: 'green'},
+  copley: {line: 'green'},
+  north_station: {line: 'orange'},
+  haymarket: {line: 'orange'},
+  park_street: {line: 'orange'},
+  state_street: {line: 'orange'},
+  downtown_crossing: {line: 'orange'},
+  chinatown: {line: 'orange'},
+  tufts_medical_center: {line: 'orange'}
+}
 
 origin_line = "red"
 
-origin_stop = "alewife"
+origin_station = "alewife"
 
 destination_line = "red"
 
-destination_stop = "South Station"
+destination_station = "South Station"
 
 lines = ["Red", "Green", "Orange"]
 
-stops = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT",
-"Park Street", "South Station"]
+# stops = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT",
+# "Park Street", "South Station"]
 
-distance_travelled =
-(stops.index(destination_stop.split.map(&:capitalize).join(' '))) -
-(stops.index(origin_stop.split.map(&:capitalize).join(' ')))
+# distance_travelled =
+# (stops.index(destination_stop.split.map(&:capitalize).join(' '))) -
+# (stops.index(origin_stop.split.map(&:capitalize).join(' ')))
 
-puts "You travelled #{distance_travelled} stops"
+# puts "You travelled #{distance_travelled} stops"
 
+current_station = origin_station
+
+
+current_station = stations.find do |station|
+  station.include?(current_station.to_sym)
+end
+
+stations.select do |key, value|
+  value.value?("red")
+end
+
+binding.pry
 
