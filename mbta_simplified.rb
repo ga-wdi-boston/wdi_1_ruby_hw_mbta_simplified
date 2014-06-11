4ee2fc1f7107633b4502c52f75caa2df81496594
Blair Caple <blair@kaaterskil.com>
2014-06-10 21:18:49 -0400

    Added distance computation

--- mbta_simplified.rb
+++ mbta_simplified.rb
@@ -32,4 +32,37 @@ until origin_station_key != nil
   print "Enter a valid station (#{select_station}): "
   origin_station_key = origin_line[:stations].find_index(gets.chomp)
 end
-binding.pry
+origin_intersection_key = origin_line[:stations].find_index(intersection)
+origin_distance = (origin_station_key - origin_intersection_key).abs
+puts
+
+# Get the destination line
+print "Enter your destination line (#{select_line}): "
+key = line_control.find_index(gets.chomp)
+until key != nil
+  print "Enter a valid destination line (#{select_line}): "
+  key = line_control.find_index(gets.chomp)
+end
+dest_line = station_control.at(key)
+puts
+
+# Get the destination station
+select_station = dest_line[:stations].join(', ')
+print "Enter your destination (#{select_station}): "
+dest_station_key = dest_line[:stations].find_index(gets.chomp)
+until origin_station_key != nil
+  print "Enter a valid destination (#{select_station}): "
+  dest_station_key = dest_line[:stations].find_index(gets.chomp)
+end
+dest_intersection_key = dest_line[:stations].find_index(intersection)
+dest_distance = (dest_station_key - dest_intersection_key).abs
+puts
+
+# Compute the total number of stops
+if origin_line != dest_line
+  tot_distance = (origin_distance + dest_distance)
+elsif
+  tot_distance = (dest_station_key - origin_station_key).abs
+end
+puts "Number of stops to travel: #{tot_distance}"
+puts
