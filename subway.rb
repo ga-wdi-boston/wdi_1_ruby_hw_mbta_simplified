dd644fccc76750e87f809e6b7f485cae092bfa35
EricRho <eric.rho.87@gmail.com>
2014-06-10 20:00:23 -0400

    Create array for Orange line including elements (stops)

--- subway.rb
+++ subway.rb
@@ -3,3 +3,6 @@ subway = {
     "Park Street", "South Station"],
   green_line: [ "Haymarket", "Government Center", "Park Street", "Boylston"
     "Arlington", "Copley"],
+  orange_line: [ "North Station", "Haymarket", "Park Street", "Downtown Crossing",
+    "Chinatown", "Tufts Medical Center"]
+}
