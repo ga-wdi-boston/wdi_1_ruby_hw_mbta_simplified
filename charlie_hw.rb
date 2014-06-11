0e1af0de6955c3e564744598854559f8c88c15c7
testkitschen <katryn.mcintosh@gmail.com>
2014-06-10 23:35:41 -0400

    Multiply journey 1 by negative 1 if on the same line
    fix style and notations
    
    updated variable numbers so they were logical
    
    That's all folks.

--- charlie_hw.rb
+++ charlie_hw.rb
@@ -1,14 +1,8 @@
 subway =
   { red: ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "Boylston"],
-  #park street
-  #8 stops
    green: ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
-  #6 stops
-  #park street = 2
    orange: ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts Medical Center"]
-  #7 stops
 }
-
 
 #Gathering necessary information via 4 prompts
 puts "What stop are you starting at?"
@@ -26,24 +20,17 @@ line_final = gets.chomp
 #Converting to calculate distance
 
 origin = subway[line_origin.downcase.to_sym]
-journey_2 = (origin.index("Park Street") - origin.index(stop_origin)).abs
+journey_1 = (origin.index("Park Street") - origin.index(stop_origin)).abs
 
 if line_origin == line_final
   destination = subway[line_final.downcase.to_sym]
-  journey_1 = destination.index("Park Street") - destination.index(stop_final)
+  journey_2 = (destination.index("Park Street") - destination.index(stop_final))*-1
 else
   destination = subway[line_final.downcase.to_sym]
-  journey_1 = (destination.index("Park Street") - destination.index(stop_final)).abs
+  journey_2 = (destination.index("Park Street") - destination.index(stop_final)).abs
 end
 
-puts journey_2
-puts journey_1
-
-distance = journey_2 + journey_1
-
+distance = journey_1 + journey_2
 
 puts "You need to go #{distance} stops to get from #{stop_origin} on the #{line_origin} line to #{stop_final} on the #{line_final} line."
 
-
-#Number of starting point
-#Number of ending point
