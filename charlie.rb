394d14b2b1e6028e50d2b116c59b82840bd665c8
sarahchristine913 <s.mele13@gmail.com>
2014-06-10 23:14:23 -0400

    Simple Distance Calculation
    * Added initial structure for taking eventual user input for trip end points w/lines
    * Gave conversion to stop input to point to keys in mta hash
    * Changed line arrays to traditional array syntax (and removed cruft commas) for multi-word stop processing
    * Created variables for stop/line matrices
    * Created conditional that runs trip distance calculation if stops are on the same line
    * Puts statements of variables for testing

--- charlie.rb
+++ charlie.rb
@@ -1,9 +1,39 @@
+require "pry"
 
+# puts "You're now Charlie on the MTA, calculate your ride stops!"
+# puts "Please enter an origin line: "
+# change to gets.chomp.downcase.to_sym once ready for
+# user input
+o_line = "Red".downcase.to_sym
 
-red_line = %w(Alewife, Davis, Porter, Harvard, Central, Kendall, Park, South Station)
+# puts "Please enter an origin stop: "
+o_stop = "Davis"
 
-green_line = %W(Haymarket, Government Center, Park, Boylston, Arlington, Copley)
+# puts "Please enter an destination line: "
+d_line = "Red".downcase.to_sym
 
-orange_line = %w(North Station, Haymarket, Park, State Street, Downtown, Chinatown, Tufts)
+# puts "Please enter an destination stop: "
+d_stop = "Harvard"
 
-mta_lines = [red_line, green_line, orange_line]
+red_line = ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall", "Park", "South Station"]
+
+green_line = ["Haymarket", "Government Center", "Park", "Boylston", "Arlington", "Copley"]
+
+orange_line = ["North Station", "Haymarket", "Park", "State", "Downtown Crossing", "Chinatown", "Tufts"]
+
+mta_lines = { red: red_line, green: green_line, orange: orange_line }
+
+# origin position, finds index value for line and stop
+
+origin_pos = mta_lines[o_line].index(o_stop)
+
+# destination position, finds index value for line and stop
+dest_pos = mta_lines[d_line].index(d_stop)
+
+if o_line == d_line
+  trip_distance = (origin_pos - dest_pos).abs
+end
+
+puts origin_pos
+puts dest_pos
+puts trip_distance
