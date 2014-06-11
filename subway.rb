f6bfb18296629d1df35d89c529391ac755508082
EricRho <eric.rho.87@gmail.com>
2014-06-10 20:01:10 -0400

    Put a space between "Alewife" and opening bracket in redline array

--- subway.rb
+++ subway.rb
@@ -1,8 +1,9 @@
 subway = {
-  red_line: ["Alewife", "Davis", "Porter", "Harvard", "Central","Kendall/MIT",
+  red_line: [ "Alewife", "Davis", "Porter", "Harvard", "Central","Kendall/MIT",
     "Park Street", "South Station"],
   green_line: [ "Haymarket", "Government Center", "Park Street", "Boylston"
     "Arlington", "Copley"],
   orange_line: [ "North Station", "Haymarket", "Park Street", "Downtown Crossing",
     "Chinatown", "Tufts Medical Center"]
 }
+
