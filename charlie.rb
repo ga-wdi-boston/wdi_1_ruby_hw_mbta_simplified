91757e16bc5db1d6523779eccc01cdb46644e49d
Emma Gardner <emmagardner@outlook.com>
2014-06-10 23:33:55 -0400

    make changes
    * make changes to input section to give error messages for invaid inputs
    
    * attempts at making egde cases using if elsif statements (not successful)

--- charlie.rb
+++ charlie.rb
@@ -1,57 +1,64 @@
-require 'pry'
 
-# Program has the subway system defined with appropriate data structures
+
+# mbta data structure: a hash including 3 keys representing a subway line with corresponding values of arrays listing their subway stops.
 mbta = {
   red: ["alewife", "davis", "porter", "harvard", "central", "kendall/MIT", "park street", "south station"],
   green: ["haymarket", "government center", "park street", "boylston", "arlington", "copley"],
   orange: ["north station", "haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]
 }
 
-binding.pry
+
 
 # obtain input data from user: origin stop, origin line, destination stop, destination line
 
 puts "What is your origin MBTA line?"
-
 o_line = gets.chomp.downcase.to_sym
+# checks to make sure user inputs valid line name
+if mbta.include?(o_line) != true
+  puts "Please choose a valid MBTA line: red, green, or orange."
+  o_line = gets.chomp.downcase.to_sym
+end
 
 puts "What is your origin MBTA stop?"
-
 o_stop = gets.chomp.downcase
+# checks to make sure user inputs valid stop name
+if mbta[o_line].include?(o_stop) != true
+  puts "Please choose a valid MBTA stop."
+  o_stop = gets.chomp.downcase
+end
 
 puts "What is your desitination line?"
-
 d_line = gets.chomp.downcase.to_sym
+# checks to make sure user inputs valid line name
+if mbta.include?(d_line) != true
+  puts "Please choose a valid MBTA line: red, green, or orange."
+  d_line = gets.chomp.downcase.to_sym
+end
 
 puts "What is your destination stop?"
-
 d_stop = gets.chomp.downcase
-
-# Take the user inputs and find/return the distance between the two stops on the same line
-
-# will list the index location of the stop on the particular line
-
-origin_index = mbta[o_line].index(o_stop)
-
-dest_index = mbta[d_line].index(d_stop)
-
-same_line_stops = (origin_index - dest_index).abs
-
-#puts "It will take to #{same_line_stops.abs} to get from #{o_stop} to #{d_stop}."
-
-binding.pry
-
-# Program can find the distance between two stops on different lines
+# checks to make sure user inputs valid stop name
+if mbta[d_line].include?(d_stop) != true
+  puts "Please choose a valid MBTA stop."
+  d_stop = gets.chomp.downcase
+end
 
 intersect = "park street"
+o_to_intersect = (mbta[o_line].index(o_stop) - mbta[o_line].index(intersect)).abs
+intersect_to_d = (mbta[d_line].index(intersect) - mbta[d_line].index(d_stop)).abs
 
-# If the o_line is different from the d_line you will have to get off at park street (bring the number of stops you have already travelled with you)
-o_to_intersect = mbta[o_line].index(o_stop) - mbta[o_line].index(intersect)
-
-#and find park street on the new line and find the number of stops from there to the d_stop.
-intersect_to_d = mbta[d_line].index(intersect) - mbta[d_line].index(d_stop)
-
-diff_line_stops = o_to_intersect + intersect_to_d
+# Take the user inputs and find/return the distance between the two stops on the SAME line
+if o_line == d_line
+# will list the index location of the stop on the particular line
+  origin_index = mbta[o_line].index(o_stop)
+  dest_index = mbta[d_line].index(d_stop)
+  same_line_stops = (origin_index - dest_index).abs
+  puts "It will take #{same_line_stops} stops to get from #{o_stop.capitalize} to #{d_stop.capitalize}."
+# Take the user inputs and find/return the distance between the two stops on DIFFERENT line
+else
+  diff_line_stops = o_to_intersect + intersect_to_d
+  puts "It will take #{diff_line_stops} stops to get from #{o_stop.capitalize} to #{d_stop.capitalize}."
+end
 
 # Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
 
