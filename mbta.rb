e032cdf264b12274603ecd89cd6630809dcc22a3
afeinber <alexandernfeinberg@gmail.com>
2014-06-10 21:37:48 -0400

    have it working for stops on the same line

--- mbta.rb
+++ mbta.rb
@@ -25,4 +25,25 @@ orange_line_stops = ['North Station',
 ]
 
 
-subway = {red_line: red_line_stops, green_line: green_line_stops, orange_line: orange_line_stops}
+subway = {red: red_line_stops, green: green_line_stops, orange: orange_line_stops}
+
+start_stop = 'Haymarket'
+end_stop = 'Downtown Crossing'
+start_line = 'Orange'.downcase.to_sym
+end_line = 'Orange'.downcase.to_sym
+
+stop_count = 0
+
+subway[start_line].rotate(subway[start_line].index(start_stop)).each do | stop |
+
+  if stop == end_stop
+    break
+  end
+
+  stop_count += 1
+
+end
+
+if stop_count > subway[start_line].length / 2
+  stop_count = subway[start_line].length - stop_count
+end
