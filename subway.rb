719d6bf77232175d4137cc2b5389a04cd375ae47
EricRho <eric.rho.87@gmail.com>
2014-06-11 00:20:09 -0400

    Create Hash "underground" with each line assigned array

--- subway.rb
+++ subway.rb
@@ -2,3 +2,7 @@ red_line = ['Alewife', 'Davis', 'Porter','Harvard','Central', 'Kendall/MIT', 'Pa
 green_line = ['Haymarket','Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
 yellow_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
 
+underground = { red: red_line,
+  green: green_line,
+  yellow: yellow_line
+}
