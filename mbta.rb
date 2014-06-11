b650bbcb012a8f2d55efdc25b99172fdf9dc40d7
afeinber <alexandernfeinberg@gmail.com>
2014-06-10 21:13:31 -0400

    Add the data structure for the subway (a hash of arrays

---
+++ mbta.rb
@@ -0,0 +1,28 @@
+
+red_line_stops = ['Alewife',
+'Davis',
+'Porter',
+'Harvard',
+'Central',
+'Kendall/MIT',
+'Park Street',
+'South Station'
+]
+green_line_stops = ['Haymarket',
+'Government Center',
+'Park Street',
+'Boylston',
+'Arlington',
+'Copley'
+]
+orange_line_stops = ['North Station',
+'Haymarket',
+'Park Street',
+'State Street',
+'Downtown Crossing',
+'Chinatown',
+'Tufts Medical Center'
+]
+
+
+subway = {red_line: red_line_stops, green_line: green_line_stops, orange_line: orange_line_stops}
