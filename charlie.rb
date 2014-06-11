01b9dad30783a38914b7bea00fba891d069c9430
Julie <juliesof@gmail.com>
2014-06-10 20:44:17 -0400

    Put 3 mbta lines into 3 separate arrays

---
+++ charlie.rb
@@ -0,0 +1,4 @@
+red = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
+green = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
+orange = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical School']
+
