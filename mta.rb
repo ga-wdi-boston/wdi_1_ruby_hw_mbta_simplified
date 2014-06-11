4aa615d117e61c16a993769d95a34d52f61b91c6
Jodi Detch <jodidetch@gmail.com>
2014-06-10 22:13:03 -0400

    Trying to figure out statement for number_of_stops_switch_lines

--- mta.rb
+++ mta.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+require 'pry'
 
 #red = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall', 'Park', 'South Station']
 #green = ['Copley', 'Arlington', 'Boylston', 'Park Street', 'Government Center', 'Haymarket']
@@ -19,21 +19,25 @@ origin_stop = "Davis".to_sym
 
 # print "Enter the destination line: "
 # change red to = gets.chomp.to_sym
-destination_line = "red".to_sym
+destination_line = "orange".to_sym
 
 # print "Enter the destination stop: "
 # change Kendall to = gets.chomp.to_sym
-destination_stop = "Kendall".to_sym
+destination_stop = "Tufts".to_sym
 
 
 # lines[origin_line] returns the array of stops on given line
 # index(origin_stop) searches for the index of the origin_stop provided
 origin_index = lines[origin_line].index("Davis")
-destination_index = lines[destination_line].index("Kendall")
+destination_index = lines[destination_line].index("Tufts")
 
 # simple case, both origin and destination stops are on the same line.
 # use abs because you don't ever want to get a negative number
 number_of_stops_same_line = (destination_index - origin_index).abs
+
+park = 0
+# more complex, origin & destination stops are on different lines
+number_of_stops_switch_lines = (park - origin_index).abs + (park - destination_index).abs
 
 if origin_line == destination_line
   puts "You have to go #{number_of_stops_same_line} stops."
@@ -41,10 +45,7 @@ else
   puts "You have to go #{number_of_stops_switch_lines} stops."
 end
 
-# more complex, origin & destination stops are on different lines
-#number_of_stops_switch_lines = (origin_index - destination_index).abs + (origin_index - destination_index).abs
-
-#binding.pry
+binding.pry
 
 
 
