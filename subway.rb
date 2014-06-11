ecca9a042b003035f01935054377f8e90d39e570
nnovak13 <nnovak@fordham.edu>
2014-06-10 20:09:25 -0400

    Added questions/.gets , along with the conditional for same-line travel

--- subway.rb
+++ subway.rb
@@ -1,18 +1,33 @@
 subway = #creates HASH for unordered lines, and nested arrays for corresponding, ordered stops
   {
-    red_line:['Alewife','Davis','Porter','Harvard','Central Kendall/MIT','Park Street','South Station'],
-    green_line:['Haymarket','Government Center','Park Street','Boylston','Arlington','Copley'],
-    orange_line:['North Station','Haymarket','Park Street','State Street','Downtown Crossing','Chinatown','Tufts Medical Center']
+    red_line:['alewife','davis','porter','harvard','central kendall/mit','park street','south station'],
+    green_line:['haymarket','government center','park street','boylston','arlington','copley'],
+    orange_line:['north station','haymarket','park street','state street','downtown crossing','chinatown','tufts medical center']
   }
 
-#puts subway[:red_line].index('Davis')
 
-distance= (subway[:red_line].index('Davis') - subway[:green_line].index('Arlington')).abs
-#distance is equal to the absoulte value, the total number of stops on a trip
+puts 'What color Origin Line would you like to take?'
+  origin_line = (gets.chomp.downcase + '_line').to_sym
+  #takes origin line color, downcases and converts to symbol to refer to specific line hash
 
 
+puts 'What is your starting stop?'
+  origin_stop = gets.chomp.downcase
+#takes stop, downcases and converts to symbol to refer to specific array string
+
+puts 'What is your Destination Line color?'
+  destination_line = (gets.chomp.downcase+ '_line').to_sym
+  #takes destination line color, downcases and converts to symbol to refer to specific line hash
+
+puts 'What is your Destination Stop?'
+  destination_stop = gets.chomp.downcase
+#takes stop, downcases and converts to symbol to refer to specific array string
+
+
+if origin_line == destination_line
+  distance= (subway[origin_line].index(origin_stop) - subway[destination_line].index(destination_stop)).abs
 puts distance
-#distance (.abs)
 
-#distance_origin_line= (subway[origin_line].index(intersection_stop) - subway[green_line:].index('Haymarket')).abs
-#distance_destination_line= (subway[origin_line].index(intersection_stop) - subway[green_line:].index('Haymarket')).abs
+#if your stops are on the same line, just evaluate the distance
+
+end
