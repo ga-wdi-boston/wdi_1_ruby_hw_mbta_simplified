796bf26403a88493de146fc50642ce6d717a524f
IljaGoushcha <iljagou@yahoo.com>
2014-09-24 19:45:32 -0400

    final version

--- subway.rb
+++ subway.rb
@@ -32,24 +32,21 @@ train_lines = {
 
 def get_line(train_lines, station)
       if train_lines[:redline].include?(station)
-            puts "redline"
+            "redline"
       elsif train_lines[:greenline].include?(station)
-            puts "greenline"
+            "greenline"
       elsif train_lines[:orangeline].include?(station)
-            puts "orangeline"
+            "orangeline"
       end
 end
 
-puts "Enter origin line:"
-origin_line = gets.chomp.to_sym
 puts "Enter origin stop:"
 origin_stop = gets.chomp
-get_line(train_lines, origin_stop)
-puts "Enter destination line:"
-destination_line = gets.chomp.to_sym
+origin_line = get_line(train_lines, origin_stop).to_sym
 puts "Enter destination stop:"
 destination_stop = gets.chomp
+destination_line = get_line(train_lines, destination_stop).to_sym
 
 leg1 = (train_lines[origin_line].index(origin_stop) - train_lines[origin_line].index(intersection)).abs
 leg2 = (train_lines[destination_line].index(intersection) - train_lines[destination_line].index(destination_stop)).abs
-puts leg1 + leg2
+puts "Your trip is " + (leg1 + leg2).to_s + " stops long."
