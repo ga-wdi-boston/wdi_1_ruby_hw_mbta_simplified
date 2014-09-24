b13851f3f15cd3dc50064966e164e7c2b3aad5a4
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-24 19:24:47 -0400

    initial commit, wrote preliminary tests

---
+++ mbta_specs.rb
@@ -0,0 +1,24 @@
+require 'mbta'
+
+describe MBTA do
+
+  before(:all) do
+  end
+
+  it "has the subway system defined with appropriate data structures" do
+
+  end
+
+  describe "#distance" do
+    it "can find the distance between two stops on the same line" do
+    end
+
+    it "can find the distance between two stops on different lines" do
+
+    end
+
+    it "gives the correct distance when starts and ends at same stop" do
+
+    end
+  end
+end
