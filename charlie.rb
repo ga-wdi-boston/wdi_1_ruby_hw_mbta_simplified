4dcb1cf6da481edbd11b68dcc1b960218936ad96
shaynaCummings <shayna.cummings@gmail.com>
2014-09-24 17:23:23 -0400

    create rb file

---
+++ charlie.rb
@@ -0,0 +1,13 @@
+# # Program has the subway system defined with appropriate data structures
+# # Program can find the distance between two stops on the same line
+# # Program can find the distance between two stops on different lines
+# # Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
+# # Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason – every line must intersect at a single common stop)
+# # Repository has several logical commits with descriptive messages
+# # Code uses correct indentation/style and descriptive variable names
+
+# Given this subway system, write a Ruby program that prompts the user for an "origin" stop and a "destination" stop, and tells them the number of stops they would have to make on that journey. For instance, traveling from Central to Copley would be 5 stops.
+
+# Hint: Prompt the user for four separate pieces of data: origin line, origin stop, destination line, and destination stop.
+
+
