44240a1b199ded4c60895034320ab6ebd2f63283
IljaGoushcha <iljagou@yahoo.com>
2014-09-24 18:04:16 -0400

    data structure ready

---
+++ subway.rb
@@ -0,0 +1,28 @@
+train_lines {
+            redline: [
+                        "Alewife",
+                        "Davis",
+                        "Porter",
+                        "Harvard",
+                        "Central",
+                        "Kendall/MIT",
+                        "Park Street",
+                        "South Station"
+            ]
+            greenline: [
+                        "Haymarket",
+                        "Government Center",
+                        "Park Street",
+                        "Boylston",
+                        "Arlington",
+                        "Copley"
+            ]
+            northstation: [
+                        "Haymarket",
+                        "Park Street",
+                        "State Street",
+                        "Downtown Crossing",
+                        "Chinatown",
+                        "Tufts Medical Center"
+            ]
+}
