d85cc4fb9ddae9907f1b9a29ede4810179d01b9d
IljaGoushcha <iljagou@yahoo.com>
2014-09-24 18:26:48 -0400

    user input created

--- subway.rb
+++ subway.rb
@@ -1,4 +1,4 @@
-train_lines {
+train_lines = {
             redline: [
                         "Alewife",
                         "Davis",
@@ -6,23 +6,35 @@ train_lines {
                         "Harvard",
                         "Central",
                         "Kendall/MIT",
-                        "Park Street",
+                        "Park Street",   #intersection
                         "South Station"
-            ]
+            ],
             greenline: [
                         "Haymarket",
                         "Government Center",
-                        "Park Street",
+                        "Park Street",  #intersection
                         "Boylston",
                         "Arlington",
                         "Copley"
-            ]
+            ],
             northstation: [
                         "Haymarket",
-                        "Park Street",
+                        "Park Street",  #intersection
                         "State Street",
                         "Downtown Crossing",
                         "Chinatown",
                         "Tufts Medical Center"
             ]
 }
+
+puts "Enter origin line:"
+origin_line = gets.chomp.to_sym
+puts "Enter origin stop:"
+origin_stop = gets.chomp
+puts "Enter destination line:"
+destination_line = gets.chomp.to_sym
+puts "Enter destination stop:"
+destination_stop = gets.chomp
+
+puts train_lines[origin_line].index(origin_stop) - train_lines[origin_line].index(origin_stop)
+
