50444db0871b9940d10dedc7e4d285ebcff58318
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-24 19:28:19 -0400

    defined stopsand lines

---
+++ mbta.rb
@@ -0,0 +1,35 @@
+class MBTA
+
+  def stops
+    {
+      red: [
+        'Alewife',
+        'Davis',
+        'Porter',
+        'Harvard',
+        'Central',
+        'Kendall/MIT',
+        'Park Street',
+        'South Station'
+      ],
+      green: [
+        'Haymarket',
+        'Government Center',
+        'Park Street',
+        'Boylston',
+        'Arlington',
+        'Copley'
+      ],
+      orange: [
+        'North Station',
+        'Haymarket',
+        'Park Street',
+        'State Street',
+        'Downtown Crossing',
+        'Chinatown',
+        'Tufts Medical Center'
+      ]
+    }
+  end
+
+end
