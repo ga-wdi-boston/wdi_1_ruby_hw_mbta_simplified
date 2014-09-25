c3540b1da8d9b3c47e523a8d52a2b7199e877cf1
Lauren Fazah <laurenfazah@gmail.com>
2014-09-24 22:33:34 -0400

    it works! done

--- mbta.rb
+++ mbta.rb
@@ -4,8 +4,6 @@
 # if they are the same, just find values for each key and subtract using absolute value
 # puts absolute value
 # if lines are not the same, find the value of each, subtract to park street of respective line and add together absolute values
-# if this doesn't work in method, remove method
-
 
 
 require 'pry'
@@ -46,7 +44,7 @@ puts "Which line will you begin on? (Red, Green, or Orange)"
 first_line = gets.chomp.downcase
 puts "Which station will you start at?"
 first_station = gets.chomp.downcase
-puts "Which line will you end on?"
+puts "Which line will you end on? (Red, Green, or Orange)"
 second_line = gets.chomp.downcase
 puts "Which station will you end at?"
 second_station = gets.chomp.downcase
@@ -64,19 +62,18 @@ def no_transfer(mbta_lines, line, stop1, stop2)
   end
 end
 
-def commute(mbta_lines, line1, station1, line2, station2)
+def commute(mbta_lines, line1, stop1, line2, stop2)
   if line1 == line2
-    no_transfer(mbta_lines, line, stop1, stop2)
+    no_transfer(mbta_lines, line1, stop1, stop2)
   else
-    stops_on_line1 = mbta_lines[line1.to_sym].index(station1) - mbta_lines[line1.to_sym].index("park street")
-    stops_on_line2 = mbta_lines[line2.to_sym].index(station2) - mbta_lines[line2.to_sym].index("park street")
+    stops_on_line1 = mbta_lines[line1.to_sym].index(stop1) - mbta_lines[line1.to_sym].index("park street")
+    stops_on_line2 = mbta_lines[line2.to_sym].index(stop2) - mbta_lines[line2.to_sym].index("park street")
     puts stops_on_line1.abs + stops_on_line2.abs
   end
 end
 
-
-
-commute(first_line, first_station, second_line, second_station)
+puts "This is how many stops long your commute will be: "
+commute(mbta_lines, first_line, first_station, second_line, second_station)
 
 
 
