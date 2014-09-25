964e1b0c39d1b113ba8951b6bb1846758be4c336
Lauren Fazah <laurenfazah@gmail.com>
2014-09-24 20:11:28 -0400

    turned all symbols into keys

--- mbta.rb
+++ mbta.rb
@@ -14,21 +14,21 @@ mbta_lines = [[["red"],
     "south station" => 8
   }]],
   [["green"],
-  [{haymarket: 1,
-    government_center: 2,
-    park_street: 3,
-    boylston: 4,
-    arlington: 5,
-    copley: 6
+  [{"haymarket" 1,
+    "government_center" 2,
+    "park_street" 3,
+    "boylston" 4,
+    "arlington" 5,
+    "copley" 6
   }]],
   [["orange"],
-  [{north_station: 1,
-    haymarket: 2,
-    park_street: 3,
-    state_street: 4,
-    downtown_crossing: 5,
-    chinatown: 6,
-    tufts_medical_center: 7
+  [{"north_station" 1,
+    "haymarket" 2,
+    "park_street" 3,
+    "state_street" 4,
+    "downtown_crossing" 5,
+    "chinatown" 6,
+    "tufts_medical_center" 7
     }]]]
 
 puts mbta_lines
