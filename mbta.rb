2ed1cbee84a4d4d15e9689b350aead395cacd5b9
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-24 20:44:57 -0400

    added basic distance calculation, 2 tests failing)

--- mbta.rb
+++ mbta.rb
@@ -36,4 +36,21 @@ class MBTA
     }
   end
 
+  def distance(stop1, stop2)
+    origin_line = stop1[0]
+    origin_stop = stop1[1]
+    destination_line = stop2[0]
+    destination_stop = stop2[1]
+
+    # the distance between a and b is (position b - position a)
+
+    if origin_line == destination_line
+      position_a = self.stops[origin_line].index(origin_stop)
+      position_b = self.stops[destination_line].index(destination_stop)
+
+      return (position_b - position_a).abs
+    end
+
+  end
+
 end
--- mbta_specs.rb
+++ mbta_specs.rb
@@ -1,25 +1,26 @@
-require 'mbta'
+require_relative 'mbta'
 
 describe MBTA do
 
   before(:all) do
+    @mbta = MBTA.new
   end
 
   it "has the subway system defined with appropriate data structures" do
-    expect(MBTA.stops[:red].is_a?(Array)).to be true
+    expect(@mbta.stops[:red].is_a?(Array)).to be true
   end
 
   describe "#distance" do
     it "can find the distance between two stops on the same line" do
-      expect(MBTA.distance([:red, 'Alewife'],[:red, 'Harvard'])).to eq 3
+      expect(@mbta.distance([:red, 'Alewife'],[:red, 'Harvard'])).to eq 3
     end
 
     it "can find the distance between two stops on different lines" do
-      expect(MBTA.distance([:red, 'Alewife'],[:red, 'Harvard'])).to eq 3
+      expect(@mbta.distance([:red, 'Alewife'],[:green, 'Boylston'])).to eq 3
     end
 
     it "gives the correct distance when starts and ends at same stop" do
-      expect(MBTA.distance([:red, 'Park Street'], [:green, 'Park Street'])).to eq 0
+      expect(@mbta.distance([:red, 'Park Street'], [:green, 'Park Street'])).to eq 0
     end
   end
 end
