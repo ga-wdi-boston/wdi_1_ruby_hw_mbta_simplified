e4314d731e45ba74c7beacaa46ac9bf1ad1bbdc3
Lauren Fazah <laurenfazah@gmail.com>
2014-09-24 20:04:58 -0400

    adding mbta.rb to github with array started

---
+++ mbta.rb
@@ -0,0 +1,35 @@
+# need a way of assigning values to stops (k/v)
+# store stops in array, stops at index
+# absolute value function to turn negative numbers into positive
+
+
+mbta_lines = [[["red"],
+  [{"alewife" => 1,
+    "davis" => 2,
+    "porter" => 3,
+    "harvard" => 4,
+    "central" => 5,
+    "kendall\/mit" => 6,
+    "park street" => 7,
+    "south station" => 8
+  }]],
+  [["green"],
+  [{haymarket: 1,
+    government_center: 2,
+    park_street: 3,
+    boylston: 4,
+    arlington: 5,
+    copley: 6
+  }]],
+  [["orange"],
+  [{north_station: 1,
+    haymarket: 2,
+    park_street: 3,
+    state_street: 4,
+    downtown_crossing: 5,
+    chinatown: 6,
+    tufts_medical_center: 7
+    }]]]
+
+puts mbta_lines
+
