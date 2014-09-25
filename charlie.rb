7d6db43a18a70688c09a5a23ac4946076b0555b1
shaynaCummings <shayna.cummings@gmail.com>
2014-09-25 00:13:24 -0400

    put things into method definitions

--- charlie.rb
+++ charlie.rb
@@ -36,41 +36,60 @@ t_lines = {
             ]
         }
 
-# prompt user for origin and destination info
-puts "I'll tell you how to go where you want to go on the MBTA!"
+def get_user_input
+  # prompt user for origin and destination info
+  puts "I'll tell you how to go where you want to go on the MBTA!"
+  puts "On which line will you originate? \n -Red \n -Orange \n -Green"
+  origin_line = gets.chomp.capitalize
+  # if origin_line != (:Red || :Orange || :Green)
+  #   puts "Please choose Red, Orange, or Green"
+  #   quit(0)
+  # end
 
-puts "On which line will you originate? \n -Red \n -Orange \n -Green"
-origin_line = gets.chomp.capitalize
-# if origin_line != (:Red || :Orange || :Green)
-#   puts "Please choose Red, Orange, or Green"
-#   quit(0)
-# end
+  puts "What's your origin stop?"
+  origin_stop = gets.chomp.capitalize
 
-puts "What's your origin stop?"
-origin_stop = gets.chomp.capitalize
+  puts "On which line is your destination? \n -Red \n -Orange \n -Green"
+  destination_line = gets.chomp.capitalize
+  # if destination_line != (:Red || :Orange || :Green)
+  #   puts "Please choose Red, Orange, or Green"
+  #   quit(0)
+  # end
 
-puts "On which line is your destination? \n -Red \n -Orange \n -Green"
-destination_line = gets.chomp.capitalize
-# if destination_line != (:Red || :Orange || :Green)
-#   puts "Please choose Red, Orange, or Green"
-#   quit(0)
-# end
+  puts "What's your destination stop?"
+  destination_stop = gets.chomp.capitalize
+end
 
-puts "What's your destination stop?"
-destination_stop = gets.chomp.capitalize
-
-
+def same_line_logic(origin_line,origin_stop,destination_line,destination_stop)
 # if origin and destintation lines are the same, we only care about finding the difference between the indexes within the array
- if origin_line == destination_line
     destin_stops = t_lines[destination_line.to_sym].index(destination_stop)
     origin_stops =  t_lines[origin_line.to_sym].index(origin_stop)
-    puts (destin_stops - origin_stops).abs
-  else
-# if origin and destination lines are not the same, we have to calculate number_of_stops in relation to park street
-  number_of_stops = (t_lines[destination_line.to_sym].index(destination_stop) - t_lines[origin_line.to_sym].index("Park Street")).abs
-    end
+    number_of_stops = (destin_stops - origin_stops).abs
+  end
+
+def diff_line_logic(origin_line,origin_stop,destination_line,destination_stop)
+  # if origin and destination lines are not the same, we have to calculate number_of_stops for each line in relation to park street
+    destin_stops = (t_lines[destination_line.to_sym].index(destination_stop) - t_lines[destination_line.to_sym].index("Park Street")).abs
+    origin_stops = (t_lines[origin_line.to_sym].index(destination_stop) - t_lines[origin_line.to_sym].index("Park Street")).abs
+    number_of_stops = (destin_stops + origin_stops)
+end
+
+def display_result(origin_line,origin_stop,destination_line,destination_stop)
+  # result displayed to user
+  puts "Your Journey:"
+  puts "Traveling from #{origin_stop} on the #{origin_line} will take #{number_of_stops} stops to get to #{destination_stop} on the #{destination_line}"
+end
+
+#program
+
+get_user_input(origin_line,origin_stop,destination_line,destination_stop)
+
+if origin_line == destination_line
+  same_line_logic
+else
+  diff_line_logic
+end
+
+display_result
 
 
-# result displayed to user
-puts "Your Journey:"
-puts "Traveling from #{origin_stop} on the #{origin_line} will take #{number_of_stops} stops to get to #{destination_stop} on the #{destination_line}"
