0ae830d087a9bf5f6ae4205e35fccdfbd3babeba
fishermanswharff <fishermanswharff@github.com>
2014-09-25 01:47:57 -0400

    new logic, much more eleganter and DRY. Flexible expressions allowed now on input, including spaces inbetween words and all lowercase. Logic now converts all input to the appropriate case and matches against the routes lines and the line stops.

--- TRouter.rb
+++ TRouter.rb
@@ -10,60 +10,45 @@ class TeeRouter
 
   def initialize(string)
     info = string.scan(/\b[\w\s\/]+/im) # => ["...", "...", "...", "..."]
-
-    #---------------------------
-    binding.pry #breakpoint
-    #---------------------------
-
     parse_array(info)
-
   end
 
   def parse_array(array)
+    # TODO: make this parsing more eleganter. Measuring for 4 isn't the best way to check for goodness of the input
     good = array.length == 4 ? parse_trip(array) : "Invalid arrival and destination."
-
-    #---------------------------
-    binding.pry #breakpoint
-    #---------------------------
-
   end
 
   def parse_trip(array)
     origins = array.map.with_index{ |orig,i| i <= 1 ? orig : nil }.compact
     destinations = array.map.with_index{ |dest,i| i > 1 ? dest : nil }.compact
 
-    # origins[0].scan(/\b[rog]/) #=> finds the first letter of the first index of origin (the departure line) to match against
+    #=> finds the first letter of the first index of all the items we need to work with
+    origin_line = origins[0].scan(/\b[rog][\w\s]+/i)[0].gsub(/\s/, "").downcase
+    destination_line = destinations[0].scan(/\b[rog][\w\s]+/i)[0].gsub(/\s/, "").downcase
+    origin = origins[1].scan(/\b[a-z][\w\s\/]+/i)[0].split.map(&:capitalize)*' '
+    destination = destinations[1].scan(/\b[a-z][\w\s\/]+/i)[0].split.map(&:capitalize)*' '
+    origin.sub!(/[\/]+[A-Za-z]+/){ $&.upcase }
+    destination.sub!(/[\/]+[A-Za-z]+/){ $&.upcase }
 
-    # determine if origin and destination are the same, if not calculate how many stops (indeces) from origin to park for transfer
-    red_start = origins[0].scan(/\b[r]/i).length ? @@routes[:redline].index(origins[1]) : nil
-    green_start = origins[0].scan(/\b[g]/i).length ? @@routes[:greenline].index(origins[1]) : nil
-    orange_start = origins[0].scan(/\b[o]/i).length ? @@routes[:orangeline].index(origins[1]) : nil
-
-    red_end = destinations[0].scan(/^[r]/i).length ? @@routes[:redline].index(destinations[1]) : nil
-    green_end = destinations[0].scan(/^[g]/i).length ? @@routes[:greenline].index(destinations[1]) : nil
-    orange_end = destinations[0].scan(/^[o]/i).length ? @@routes[:orangeline].index(destinations[1]) : nil
-
-    same_line(red_start, red_end, array) if red_start && red_end
-    same_line(green_start, green_end, array) if green_start && green_end
-    same_line(orange_start, orange_end, array) if orange_start && green_end
-
+    route(@@routes[origin_line.to_sym], origin,@@routes[destination_line.to_sym], destination, array)
   end
 
-  def same_line(start_station,end_station,array)
-    #---------------------------
-    binding.pry #breakpoint
-    #---------------------------
-    puts "Get on the #{array[0]} at #{array[1]} station, ride for #{end_station - start_station} stop(s) and get off at #{array[-1]}"
-  end
-
-  def with_stops(origs,dests)
-    # logic goes here to calculate the number of stops between 2 lines
+  def route(line1, origin, line2, destination, input)
 
     #---------------------------
     binding.pry #breakpoint
     #---------------------------
 
-    puts "Tough shit kiddo. Gotta change trains at park street"
+    if line1 == line2
+      result = "Get on the #{input[0]} line, ride it for #{line2.index(destination)-line1.index(origin)} stops, and get off at #{input[-1]}"
+    elsif origin == destination
+      result = "You want to get on and off at the same location? Doesn't sound like much of a trip to me..."
+    end
+
+    #---------------------------
+    binding.pry #breakpoint
+    #---------------------------
+
 
   end
 
@@ -72,13 +57,13 @@ end
 puts "Enter: departure line, departure station, arrival line, arrival station (comma separated values)"
 TeeRouter.new(gets.chomp)
 
-# red line, Harvard, Redline, Central
+# redline, harvard, Red line, kendall/mit
 
 =begin
 Specifications
 ————————————————————————————————————————————————————————————————————
 * Program has the subway system defined with appropriate data structures ##Done
-* Program can find the distance between two stops on the same line
+* Program can find the distance between two stops on the same line ##Done
 * Program can find the distance between two stops on different lines
 * Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
 * Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason – every line must intersect at a single common stop)
