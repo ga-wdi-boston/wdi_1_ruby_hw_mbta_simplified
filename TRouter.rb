0cbc69bd093d76c3fa5ed8bce3ff4afb5665b5cb
fishermanswharff <fishermanswharff@github.com>
2014-09-24 22:59:59 -0400

    checks for proper directions, then parses if ok. looking for 4 comma separated values in input from the user

--- TRouter.rb
+++ TRouter.rb
@@ -10,14 +10,17 @@ class TeeRouter
 
   def initialize(string)
     info = string.scan(/[\w]+/im) # => ["...", "...", "...", "..."]
-    parse_destinations(info)
+    parse_array(info)
   end
 
-  def parse_destinations(array)
+  def parse_array(array)
+    good = array.length == 4 ? parse_trip(array) : "Invalid arrival and destination."
+  end
+
+  def parse_trip(array)
+    origins = array.map.with_index{ |orig,i| i <= 1 ? orig : nil }.compact
+    destinations = array.map.with_index{ |dest,i| i > 1 ? dest : nil }.compact
     binding.pry #breakpoint
-
-
-
   end
 
 end
@@ -26,3 +29,16 @@ puts "Enter: departure line, departure station, arrival line, arrival station (c
 TeeRouter.new(gets.chomp)
 
 # Redline, Harvard, OrangeLine, Chinatown
+
+=begin
+Specifications
+————————————————————————————————————————————————————————————————————
+* Program has the subway system defined with appropriate data structures ##Done
+* Program can find the distance between two stops on the same line
+* Program can find the distance between two stops on different lines
+* Program gives the correct distance in edge cases (e.g. route starts and ends at the same stop, route starts or ends at Park Street)
+* Stops and lines can be added, removed, or rearranged in your code without making any changes to the route-finding logic (within reason – every line must intersect at a single common stop)
+* Repository has several logical commits with descriptive messages
+* Code uses correct indentation/style and descriptive variable names
+
+=end
