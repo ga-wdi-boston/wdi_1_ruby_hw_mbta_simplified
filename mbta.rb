d7d068bf79cad23d93100a8c6b4022af42ccd23c
Lauren Fazah <laurenfazah@gmail.com>
2014-09-24 20:16:23 -0400

    added prompts and gets.chonmp for user's two stations

--- mbta.rb
+++ mbta.rb
@@ -2,6 +2,8 @@
 # store stops in array, stops at index
 # absolute value function to turn negative numbers into positive
 
+require 'pry'
+
 
 mbta_lines = [[["red"],
   [{"alewife" => 1,
@@ -15,21 +17,25 @@ mbta_lines = [[["red"],
   }]],
   [["green"],
   [{"haymarket" 1,
-    "government_center" 2,
-    "park_street" 3,
+    "government center" 2,
+    "park street" 3,
     "boylston" 4,
     "arlington" 5,
     "copley" 6
   }]],
   [["orange"],
-  [{"north_station" 1,
+  [{"north station" 1,
     "haymarket" 2,
-    "park_street" 3,
-    "state_street" 4,
-    "downtown_crossing" 5,
+    "park street" 3,
+    "state street" 4,
+    "downtown crossing" 5,
     "chinatown" 6,
-    "tufts_medical_center" 7
+    "tufts medical center" 7
     }]]]
 
-puts mbta_lines
-
+puts "Hi there! I'd love to help you figure"
+puts "out how far your commute is."
+puts "Which station will you start at?"
+first_station = gets.chomp.downcase
+puts "Which station will you end at?"
+second_station = gets.chomp.downcase
