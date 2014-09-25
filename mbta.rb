b8d0e7aed70be00b62c468e27336e80356b5492f
Lauren Fazah <laurenfazah@gmail.com>
2014-09-24 21:34:01 -0400

    big changes. array of hashes wasn't working, so i flipped to a hash of arrays for mbta stops.

--- mbta.rb
+++ mbta.rb
@@ -5,33 +5,33 @@
 require 'pry'
 
 
-mbta_lines = [[["red"],
-  [{"alewife" => 1,
-    "davis" => 2,
-    "porter" => 3,
-    "harvard" => 4,
-    "central" => 5,
-    "kendall\/mit" => 6,
-    "park street" => 7,
-    "south station" => 8
-  }]],
-  [["green"],
-  [{"haymarket" => 1,
-    "government center" => 2,
-    "park street" => 3,
-    "boylston" => 4,
-    "arlington" => 5,
-    "copley" => 6
-  }]],
-  [["orange"],
-  [{"north station" => 1,
-    "haymarket" => 2,
-    "park street" => 3,
-    "state street" => 4,
-    "downtown crossing" => 5,
-    "chinatown" => 6,
-    "tufts medical center" => 7
-    }]]]
+mbta_lines = {
+  red:
+  ["alewife",
+    "davis",
+    "porter",
+    "harvard",
+    "central",
+    "kendall\/mit",
+    "park street",
+    "south station"],
+  green:
+  ["haymarket",
+    "government center",
+    "park street",
+    "boylston",
+    "arlington",
+    "copley"],
+  orange:
+  ["north station",
+    "haymarket",
+    "park street",
+    "state street",
+    "downtown crossing",
+    "chinatown",
+    "tufts medical center"]
+  }
+
 
 puts "Hi there! I'd love to help you figure out how far your commute is."
 
@@ -45,19 +45,38 @@ puts "Which station will you end at?"
 second_station = gets.chomp.downcase
 
 
-def commute(first_line, first_station, second_line, second_station)
+#def commute(first_line, first_station, second_line, second_station)
+  # setting index numbers for each station to navigate in array
+  # if first_line == "red"
+  #   first_line = 0
+  # elsif first_line == "green"
+  #   first_line = 1
+  # elsif first_line == "orange"
+  #   first_line = 2
+  # elsif second_line == "red"
+  #   second_line = 0
+  # elsif second_line == "green"
+  #   second_line = 1
+  # elsif second_line == "orange"
+  #   second_line = 2
+  # end
+
+puts mbta_lines[0][red]["davis"]
+
+#   num_of_stops_single = mbta_lines[first_line][first_station].to_i - mbta_lines[first_line][second_station].to_i
+
+# puts num_of_stops_single
+
+# binding.pry
 
 
-if first_line == second_line
-  puts
+
   # if they are the same, just find values for each key and subtract using absolute value
   # puts absolute value
 
   # if lines are not the same, find the value of each, subtract to park street of respective line and add together absolute values
 
-
-
-
+  # if this doesn't work in method, remove method
 
 
 
