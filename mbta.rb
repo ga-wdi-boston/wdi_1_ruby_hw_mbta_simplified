57bfa65c661f2a89038f62c94392350998c6b8e4
raielin@gmail.com <raielin@gmail.com>
2014-09-24 23:15:56 -0400

    created methods for user prompts for T line and stop number.

--- mbta.rb
+++ mbta.rb
@@ -53,9 +53,38 @@ Use methods to make your code better, with each method doing a single and small 
 
 require 'pry'
 
-def list_stops(t_line_color)
+# def travel_distance(origin_line, origin_stop, destination_line, destination_stop)
 
+#   if origin_line == destination_line
+
+
+
+#   puts "You will be traveling #{travel_distance} stops in total." # where transfer is necessary, would also like to output number of stops to and from Park on each line, but not enough time.
+
+
+# end
+
+
+def get_user_line(on_or_off)
+  if on_or_off == "origin"
+    print "What is the color of the T line you are STARTING on: "
+  end
+
+  if on_or_off == "destination"
+    print "\nWhat is the color of the T line you are ENDING on: "
+  end
+    gets.chomp.downcase
+
+  # ideally should include a test for input validity, but am saving time
 end
+
+def get_user_stop
+    print "\nYour stop number is: "
+    gets.chomp.downcase
+
+    # ideally should include a test for input validity, but am saving time
+end
+
 
 # thinking maybe there's a way to take user's selection and modify/shift arrays so that user's stop becomes 0 in the indices
 mbta = {
@@ -93,38 +122,33 @@ mbta = {
 
 # FOUR USER INPUTS: origin_line, origin_stop, destination_line, destination_stop
 
-puts "\nLet me tell you how many stops you will be traveling on this trip.\n"
+puts "\nLet me tell you how many T stops you'll need to travel on your ride.\n"
 
-print "First, what is the color of the T line you are STARTING on: "
-origin_line = gets.chomp.downcase
-# ideally should include a test for input validity
+binding.pry
 
-puts "\nNext, what specific #{origin_line.capitalize} Line stop you are getting ON at.\n"
+origin_line = get_user_line("origin")
 
-# would like to create a method to do this and then only need to call the method in the main code, but couldn't figure it out in the time I had. Main question is how to refer to mbta hash within the method -- guessing hash itself has to be its own separate method or class somehow?
+# would like to create a method to do this so can just call the method in the main code, but couldn't figure it out in time. Guessing hash itself has to be its own separate method or class somehow?
 all_origin_stops = mbta[origin_line.to_sym].map do |stop|
   i = mbta[origin_line.to_sym].index(stop)
   "Type #{i} for #{stop}."
 end
 
 puts all_origin_stops
-print "\nYour stop number is: "
-origin_stop = gets.chomp.to_i
-# ideally should include a test for input validity
 
-print "\nAlso, what is the color of the T line you are ENDING on: "
-destination_line = gets.chomp.downcase
-# ideally should include a test for input validity
+origin_stop = get_user_stop
 
-puts "\nAnd finally, what specific #{destination_line.capitalize} Line stop you are getting OFF at.\n"
+destination_line = get_user_line("destination")
+
+puts "\nWhat specific #{destination_line.capitalize} Line stop you are getting OFF at.\n"
 all_destination_stops = mbta[destination_line.to_sym].map do |stop|
   i = mbta[destination_line.to_sym].index(stop)
   "Type #{i} for #{stop}."
 end
 
 puts all_destination_stops
-print "\nYour stop number is: "
-destination_stop = gets.chomp.to_i
+
+destination_stop = get_user_stop
 
 binding.pry
 
