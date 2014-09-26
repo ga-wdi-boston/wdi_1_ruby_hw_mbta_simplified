52bb73b4c845606937d0cb0c74a81689d56a4ba6
shaynaCummings <shayna.cummings@gmail.com>
2014-09-26 18:05:08 -0400

    fixed symbol mismatch issue so now it will actually run

--- charlie.rb
+++ charlie.rb
@@ -6,90 +6,108 @@
 
 require 'pry'
 
+
 t_lines = {
-          red_line: [
-            "Alewife",
-            "Davis",
-            "Porter",
-            "Harvard",
-            "Central",
-            "Kendall/MIT",
-            "Park Street",
-            "South Station"
+          red: [
+            "alewife",
+            "davis",
+            "porter",
+            "harvard",
+            "kentral",
+            "kendall/mit",
+            "park street",
+            "south station"
             ],
-          green_line: [
-            "Haymarket",
-            "Government Center",
-            "Park Street",
-            "Boylston",
-            "Arlington",
-            "Copley"
+          green: [
+            "haymarket",
+            "government center",
+            "park street",
+            "boylston",
+            "arlington",
+            "copley"
             ],
-          orange_line: [
-            "North Station",
-            "Haymarket",
-            "Park Street",
-            "State Street",
-            "Downtown Crossing",
-            "Chinatown",
-            "Tufts Medical Center"
+          orange: [
+            "north station",
+            "haymarket",
+            "park street",
+            "state street",
+            "downtown crossing",
+            "chinatown",
+            "tufts medical center"
             ]
         }
+# end
 
-def get_user_input
-  # prompt user for origin and destination info
+# Get user input
+def get_origin_line
+
   puts "I'll tell you how to go where you want to go on the MBTA!"
   puts "On which line will you originate? \n -Red \n -Orange \n -Green"
-  origin_line = gets.chomp.capitalize
-  # if origin_line != (:Red || :Orange || :Green)
+  origin_line = gets.chomp.downcase.to_sym
+
+  # Error checking
+  # if origin_line != (:red || :orange || :green)
   #   puts "Please choose Red, Orange, or Green"
   #   quit(0)
+  # else
+  #   origin_line
   # end
+end
+
+def get_origin_stop
 
   puts "What's your origin stop?"
-  origin_stop = gets.chomp.capitalize
+  origin_stop = gets.chomp.downcase
+
+end
+
+def get_destination_line
 
   puts "On which line is your destination? \n -Red \n -Orange \n -Green"
-  destination_line = gets.chomp.capitalize
-  # if destination_line != (:Red || :Orange || :Green)
+  destination_line = gets.chomp.downcase.to_sym
+
+  # Error checking
+  # if destination_line != (:red || :orange || :green)
   #   puts "Please choose Red, Orange, or Green"
   #   quit(0)
+  # else
+  #   destination_line
   # end
 
+end
+
+def get_destination_stop
+
   puts "What's your destination stop?"
-  destination_stop = gets.chomp.capitalize
+
+  destination_stop = gets.chomp.downcase
 end
 
-def same_line_logic(origin_line,origin_stop,destination_line,destination_stop)
-# if origin and destintation lines are the same, we only care about finding the difference between the indexes within the array
-    destin_stops = t_lines[destination_line.to_sym].index(destination_stop)
-    origin_stops =  t_lines[origin_line.to_sym].index(origin_stop)
-    number_of_stops = (destin_stops - origin_stops).abs
-  end
+def line_logic(subway_map,origin_line,origin_stop,destination_line,destination_stop)
 
-def diff_line_logic(origin_line,origin_stop,destination_line,destination_stop)
-  # if origin and destination lines are not the same, we have to calculate number_of_stops for each line in relation to park street
-    destin_stops = (t_lines[destination_line.to_sym].index(destination_stop) - t_lines[destination_line.to_sym].index("Park Street")).abs
-    origin_stops = (t_lines[origin_line.to_sym].index(destination_stop) - t_lines[origin_line.to_sym].index("Park Street")).abs
-    number_of_stops = (destin_stops + origin_stops)
+  # This gets us from our origin to Park Street
+
+  part1 = (subway_map[origin_line].index("park street") - subway_map[origin_line].index(origin_stop)).abs
+
+  # This gets us from Park St to destination
+  part2 = (subway_map[destination_line].index("park street") - subway_map[destination_line].index(destination_stop)).abs
+
+  number_of_stops = part1 + part2
+
+  "Traveling from #{origin_stop.capitalize} on the #{origin_line.capitalize} line to #{destination_stop.capitalize} on the #{destination_line.capitalize} line will take #{number_of_stops} stops."
+
 end
 
-def display_result(origin_line,origin_stop,destination_line,destination_stop)
-  # result displayed to user
+
+# Display result to user
+def display_result(origin_line,origin_stop,destination_line,destination_stop,number_of_stops)
+
   puts "Your Journey:"
-  puts "Traveling from #{origin_stop} on the #{origin_line} will take #{number_of_stops} stops to get to #{destination_stop} on the #{destination_line}"
+  puts "Traveling from #{origin_stop} on the #{origin_line} line go to #{destination_stop} on the #{destination_line }will take #{number_of_stops} stops."
 end
 
 #program
 
-get_user_input(origin_line,origin_stop,destination_line,destination_stop)
 
-if origin_line == destination_line
-  same_line_logic
-else
-  diff_line_logic
-end
-
-display_result
-
-
+puts line_logic(t_lines,get_origin_line,get_origin_stop,get_destination_line, get_destination_stop)
+# puts display_result
