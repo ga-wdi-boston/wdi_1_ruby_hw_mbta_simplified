d968bcf4f163ef68c165250dcadbe7323b8651ca
Rebecca Paz <r.pazyaquian@gmail.com>
2014-09-26 17:00:55 -0400

    refactored distance, created #trip_positions, working on more refactoring

--- mbta.rb
+++ mbta.rb
@@ -49,29 +49,57 @@ class MBTA
     }
   end
 
+  # what if trip WASN'T a hash?
+
   def position(line, stop)
     self.stops[line].index(stop)
   end
 
+  def trip_positions(trip)
+    # origin = self.position(trip[:origin][:line], trip[:origin][:stop])
+    # destination = self.position(trip[:destination][:line], trip[:destination][:stop])
+    # [origin, destination]
+
+    # instead, do something like
+    positions = [trip[:origin], trip[:destination]].map do |point|
+      self.position(point[:line], point[:stop])
+    end
+    positions
+
+  end
+
   def distance(trip)
-    origin_line = trip[:origin][:line]
-    origin_stop = trip[:origin][:stop]
-    destination_line = trip[:destination][:line]
-    destination_stop = trip[:destination][:stop]
 
-    position_a = self.position(origin_line, origin_stop)
-    position_b = self.position(destination_line, destination_stop)
+    # why is this so fucking huge?
 
+    # first, you get the origin line, the origin stop,
+    # the destination line, and the destination stop
+
+    origin_line, origin_stop = trip[:origin].values
+    destination_line, destination_stop = trip[:destination].values
+
+    # i'd like something like this:
+
+    position_a, position_b = self.trip_positions(trip)
+
+    # v--- this is the only code that actually concerns distance
     if origin_line == destination_line
       return (position_b - position_a).abs
     else
-      origin_trip = self.trip(origin_line, origin_stop, origin_line, self.common_stop)
-      destination_trip = self.trip(destination_line, self.common_stop, destination_line, destination_stop)
+      origin_trip = self.trip(origin_line,
+        origin_stop,
+        origin_line,
+        self.common_stop)
+      destination_trip = self.trip(destination_line,
+        self.common_stop,
+        destination_line,
+        destination_stop)
 
       distance_a = distance(origin_trip)
       distance_b = distance(destination_trip)
       return distance_a + distance_b
     end
+    # ^---
 
   end
 
