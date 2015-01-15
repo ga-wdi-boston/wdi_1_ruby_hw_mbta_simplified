594db974b3edceb30c0db96b2f7f3d369611be5f
Dan Stoppelman <dstop75@gmail.com>
2015-01-15 00:17:13 -0500

    Add lib/ and mbta.rb, and add mbta hash to mbta.rb

---
+++ mbta.rb
@@ -0,0 +1,35 @@
+mbta = {
+        red: {
+          stations: [
+            "Alewife",
+            "Davis",
+            "Porter",
+            "Harvard",
+            "Central",
+            "Kendall/MIT",
+            "Park Street",
+            "South Station"
+          ]
+        },
+        green: {
+          stations: [
+            "Haymarket",
+            "Government Center",
+            "Park Street",
+            "Boylston",
+            "Arlington",
+            "Copley"
+          ]
+        },
+        orange: {
+          stations: [
+            "North Station",
+            "Haymarket",
+            "Park Street",
+            "State Street",
+            "Downtown Crossing",
+            "Chinatown",
+            "Tufts"
+          ]
+        }
+}
