0faec5270bcb71a0576f91eaeae74be66f8bd9d9
lady3bean <annehawleybrett@gmail.com>
2015-01-14 22:16:57 -0500

    Initial Map of the dream MBTA

---
+++ mbta.rb
@@ -0,0 +1,32 @@
+mbtamap = {
+  "Red Line" =>[
+    "Alewife",
+    "Davis",
+    "Porter",
+    "Harvard",
+    "Central",
+    "Kendall/MIT",
+    "Park Street",
+    "South Station"
+  ],
+  "Green Line" => [
+    "Haymarket",
+    "Government Center",
+    "Park Street",
+    "Boylston",
+    "Arlington",
+    "Copley"
+  ],
+  "Orange Line" => [
+    "North Station",
+    "Haymarket",
+    "Park Street",
+    "State Street",
+    "Downtown Crossing",
+    "Chinatown",
+    "Tufts Medical Center"
+  ]
+}
+
+puts mbtamap.keys
+puts mbtamap["Red Line"]
