91f534f0f90cc49b1291f6c5d41987702e9d205a
Dan Stoppelman <dstop75@gmail.com>
2015-01-15 00:22:09 -0500

    Add method to mbta.rb

--- mbta.rb
+++ mbta.rb
@@ -37,3 +37,11 @@ mbta = {
 def calc_index_distance(array, index_a, index_b)
   ( array.index(index_a) - array.index(index_b) ).abs
 end
+
+def calc_stops(system, origin_line, origin, destination_line, destination)
+  if origin_line == destination_line
+    calc_index_distance(system[origin_line][:stations], origin, destination)
+  else
+    calc_index_distance(system[origin_line][:stations], origin, "Park Street") + calc_index_distance(system[destination_line][:stations], destination, "Park Street")
+  end
+end
