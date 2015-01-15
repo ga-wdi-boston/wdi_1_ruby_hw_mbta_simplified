b12c21d6d7ee760a300b409c03de8405434a7e9d
Anthony Broccoli <acbrocco@syr.edu>
2015-01-15 10:11:06 -0500

    added a get_mbta method and fixed the code for mbta.rb and spec_mbta.rb

--- mbta.rb
+++ mbta.rb
@@ -1,16 +1,21 @@
-mbta = [
-["haymarket" , "government_center" ,"park street" ,"boylston" ,"arlington" , "copley" ],
-["alewife", "davis", "porter", "harvard", "central", "kendall", "park street", "south station" ],
-["north station","haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]]
+require 'byebug'
 
+def get_mbta(line)
+  mbta = [
+  ["haymarket" , "government center" ,"park street" ,"boylston" ,"arlington" , "copley" ],
+  ["alewife", "davis", "porter", "harvard", "central", "kendall", "park street", "south station" ],
+  ["north station","haymarket", "park street", "state street", "downtown crossing", "chinatown", "tufts medical center"]]
+
+  mbta[line]
+end
 
 def distance_same_line (line, start, stop)
-  if line == "green line"
-    line = mbta[0]
-  elsif line == "red line"
-    line = mbta[1]
+  if line == "greenline"
+    line = get_mbta(0)
+  elsif line == "redline"
+    line = get_mbta(1)
   else
-    line = mbta[2]
+    line = get_mbta(2)
   end
 
   (line.index(start) - line.index(stop)).abs
@@ -19,25 +24,26 @@ end
 
 
 def distance_other_line (start_line, end_line, start_stop, end_stop)
-  if start_line == "green line"
-    start_line = mbta[0]
-  elsif start_line == "red line"
-    start_line = mbta[1]
+
+  if start_line == "greenline"
+    start_line = get_mbta(0)
+  elsif start_line == "redline"
+    start_line = get_mbta(1)
   else
-    start_line = mbta[2]
+    start_line = get_mbta(2)
   end
 
-  if end_line == "green line"
-    end_line = mbta[0]
-  elsif end_line == "red line"
-    end_line = mbta[1]
+  if end_line == "greenline"
+    end_line = get_mbta(0)
+  elsif end_line == "redline"
+    end_line = get_mbta(1)
   else
-    end_line = mbta[2]
+    end_line = get_mbta(2)
   end
 
-  first_line = (start_line.index("park_st") - (start_line.index(start_stop))).abs
-  second_line = (end_line.index("park_st") - (end_line.index(end_stop))).abs
-  distance = first_line + second_line + 1
+  first_line = (start_line.index("park street") - start_line.index(start_stop)).abs
+  second_line = (end_line.index("park street") - end_line.index(end_stop)).abs
+  distance = first_line + second_line
 end
 
 
--- spec/spec_mbta.rb
+++ spec/spec_mbta.rb
@@ -1,20 +1,18 @@
 require_relative '../mbta.rb'
 
-
-
 RSpec.describe '#distance_same_line' do
   it 'returns the number of stops from origin stop to final stop on the same line' do
-    expect(distance_same_line("green line", "haymarket", "boylston")).to eq (3)
-    expect(distance_same_line("red line", "alewife", "central")).to eq (4)
-    expect(distance_same_line("orange line", "tufts medical center", "haymarket")).to eq (5)
+    expect(distance_same_line("greenline", "haymarket", "boylston")).to eq (3)
+    expect(distance_same_line("redline", "alewife", "central")).to eq (4)
+    expect(distance_same_line("orangeline", "tufts medical center", "haymarket")).to eq (5)
   end
 end
 
 
 RSpec.describe '#distance_other_line' do
   it 'returns the number of stops from origin stop to final stop on the same line' do
-    expect(distance_other_line("green line", "red line", "haymarket", "harvard")).to eq (5)
-    expect(distance_other_line("red line", "orange line", "alewife", "state street")).to eq (7)
-    expect(distance_other_line("orange line", "green line", "tufts medical center", "haymarket")).to eq (6)
+    expect(distance_other_line("greenline", "redline", "haymarket", "harvard")).to eq (5)
+    expect(distance_other_line("redline", "orangeline", "alewife", "state street")).to eq (7)
+    expect(distance_other_line("orangeline", "greenline", "tufts medical center", "haymarket")).to eq (6)
   end
 end
