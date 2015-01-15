2eb9614f8aa9487ba7d92cb6c99630abacf32c0f
Annie <anne.bourgeois1@gmail.com>
2015-01-14 23:16:03 -0500

    Added some comments with questions

--- mbta.rb
+++ mbta.rb
@@ -37,7 +37,6 @@ puts mbta[origin_line]
 puts "What is your stop of origin?"
 origin_stop = gets.chomp
 
-# Get the index for the origin_stop
 origin_stop_index = mbta[origin_line].find_index(origin_stop)
 
 puts "What is your destination line?"
@@ -48,23 +47,18 @@ puts mbta[destination_line]
 puts "What is your destination stop?"
 destination_stop = gets.chomp
 
-# Get the index for the destination stop
 destination_stop_index = mbta[destination_line].find_index(destination_stop)
 
-# Find out the indexes for Park Street
 origin_park_index = mbta[origin_line].find_index("Park Street")
 destination_park_index = mbta[origin_line].find_index("Park Street")
 
-# Check to see if origin line is the same as destination line.
-# If so, you only need to subtract origin_stop_index from destination_stop_index
-# If not the same origin_line, You need to subtract park_street_index from origin_stop_index
-# and then subtract destination_park_index from destination_stop_index
-# Once you have the number of stops on each line, add them together
+# Which of these lines is better code? The one referencing variables or the one evaluating on the fly?
 if origin_line == destination_line
+ # number_of_stops = (mbta[origin_line].find_index(origin_stop) - mbta[destination_line].find_index(destination_stop)).abs
   number_of_stops = (origin_stop_index - destination_stop_index).abs
 else
+  # number_of_stops = ((mbta[origin_line].find_index(origin_stop) - mbta[origin_line].find_index("Park Street")).abs + (mbta[destination_line].find_index(destination_stop) - mbta[origin_line].find_index("Park Street")).abs)
   number_of_stops = ((origin_stop_index - origin_park_index).abs + (destination_stop_index - destination_park_index).abs)
 end
-
 
 puts "It will take #{number_of_stops} stops to get from the #{origin_line} line: #{origin_stop} to #{destination_line} line: #{destination_stop}."
